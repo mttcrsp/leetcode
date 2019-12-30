b0a840d22a4c1f8803b708c1676fca4dc7949bc4
mttcrsp <crespi.matteo@icloud.com>
2019-12-30 13:48:34 +0100

    Initial commit

---
+++ Sources/pascals-triangle.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func generate(_ numRows: Int) -> [[Int]] {
+        guard numRows >= 0 else {
+            return [[1]]
+        }
+
+        if numRows == 0 {
+            return []
+        }
+
+        var pyramid = (1 ... numRows).map { count in
+            [Int](repeating: 1, count: count)
+        }
+
+        guard numRows > 1 else {
+            return pyramid
+        }
+
+        for row in 1 ..< numRows - 1 {
+            for (column, (lhs, rhs)) in zip(pyramid[row], pyramid[row].dropFirst()).enumerated() {
+                pyramid[row + 1][column + 1] = lhs + rhs
+            }
+        }
+
+        return pyramid
+    }
+}
