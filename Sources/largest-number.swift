5658152a576645f8fa0fea156e7c24f629b74a60
mttcrsp <crespi.matteo@icloud.com>
2019-12-30 19:41:02 +0100

    179. Largest Number

---
+++ Sources/largest-number.swift
@@ -0,0 +1,9 @@
+extension Solution {
+    func largestNumber(_ numbers: [Int]) -> String {
+        guard !numbers.allSatisfy({ number in number == 0 }) else { return "0" }
+
+        var strings = numbers.map { number in String(number) }
+        strings.sort { lhs, rhs in lhs + rhs > rhs + lhs }
+        return strings.joined()
+    }
+}
