02b263bec98a4669d4f119c892d9a79418ab2a8c
mttcrsp <crespi.matteo@icloud.com>
2019-12-31 19:42:07 +0100

    551. Student Attendance Record I

---
+++ Sources/student-attendance-record-i.swift
@@ -0,0 +1,31 @@
+extension Solution {
+    func checkRecord(_ attendance: String) -> Bool {
+        let attendance = Array(attendance)
+        var wasAbsenceRecorded = false
+
+        for day in attendance.indices {
+            let record = attendance[day]
+
+            switch record {
+            case "A" where wasAbsenceRecorded:
+                return false
+            case "A" where !wasAbsenceRecorded:
+                wasAbsenceRecorded = true
+            case "L":
+                let previousDay1 = day - 1
+                let previousDay2 = day - 2
+                guard attendance.indices.contains(previousDay2) else { continue }
+                let previousRecord1 = attendance[previousDay1]
+                let previousRecord2 = attendance[previousDay2]
+                guard previousRecord1 == "L", previousRecord2 == "L" else { continue }
+                return false
+            case "P":
+                break
+            default:
+                preconditionFailure("Unexpected character found '\(record)'")
+            }
+        }
+
+        return true
+    }
+}
