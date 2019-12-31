9bcc397f3ecb46a6a4e7ce5fd26d9a8b0b220aa0
mttcrsp <crespi.matteo@icloud.com>
2019-12-31 01:42:26 +0100

    332. Reconstruct Itinerary

---
+++ Sources/reconstruct-itinerary.swift
@@ -0,0 +1,27 @@
+extension Solution {
+    func findItinerary(_ tickets: [[String]]) -> [String] {
+        guard !tickets.isEmpty else { return [] }
+
+        var destinationsForOrigin: [String: [String]] = [:]
+        for ticket in tickets {
+            let origin = ticket[0], destination = ticket[1]
+            destinationsForOrigin[origin, default: []].append(destination)
+        }
+
+        for (origin, destinations) in destinationsForOrigin {
+            destinationsForOrigin[origin] = destinations.sorted()
+        }
+
+        var dfs: [String] = ["JFK"]
+        var itinerary: [String] = []
+        while let previousAirport = dfs.last {
+            if destinationsForOrigin[previousAirport, default: []].isEmpty {
+                itinerary.append(previousAirport)
+                dfs.removeLast()
+            } else {
+                dfs.append(destinationsForOrigin[previousAirport]!.removeFirst())
+            }
+        }
+        return itinerary.reversed()
+    }
+}
