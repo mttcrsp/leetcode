b6b445399cf0613d5ec78a00d7448ffe7e729313
mttcrsp <crespi.matteo@icloud.com>
2020-03-02 23:35:52 +0100

    58. Length of Last Word

---
+++ Sources/length-of-last-word.swift
@@ -0,0 +1,13 @@
+extension Solution {
+    func lengthOfLastWord(_ s: String) -> Int {
+        var string = s
+        while string.last == " " {
+            string.removeLast()
+        }
+
+        for (index, character) in string.reversed().enumerated() where character == " " {
+            return index
+        }
+        return string.count
+    }
+}
