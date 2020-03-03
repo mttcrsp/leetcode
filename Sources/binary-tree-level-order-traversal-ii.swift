60975f381e4b246eb1c50bd3c63ef113b405625c
mttcrsp <crespi.matteo@icloud.com>
2020-03-03 01:37:19 +0100

    107. Binary Tree Level Order Traversal II

---
+++ Sources/binary-tree-level-order-traversal-ii.swift
@@ -0,0 +1,25 @@
+extension Solution {
+    func levelOrderBottom(_ root: TreeNode?) -> [[Int]] {
+        var valuesForDepth: [Int: [Int]] = [:]
+
+        func visit(_ node: TreeNode?, depth: Int) {
+            guard let node = node else { return }
+
+            visit(node.left, depth: depth + 1)
+            visit(node.right, depth: depth + 1)
+            valuesForDepth[depth, default: []].append(node.val)
+        }
+
+        visit(root, depth: 0)
+
+        let sortedLevels = valuesForDepth.sorted { lhs, rhs in
+            lhs.key > rhs.key
+        }
+
+        var result: [[Int]] = []
+        for (_, values) in sortedLevels {
+            result.append(values)
+        }
+        return result
+    }
+}
