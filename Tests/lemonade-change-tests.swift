7601715785566a38b0580ad9905e89718de64927
mttcrsp <crespi.matteo@icloud.com>
2020-03-03 22:23:11 +0100

    860. Lemonade Change

---
+++ Tests/lemonade-change-tests.swift
@@ -0,0 +1,35 @@
+@testable
+import Leetcode
+import XCTest
+
+extension SolutionTests {
+    func testLemonadeChange1() {
+        let input = [5, 5, 5, 10, 20]
+        let output = true
+        XCTAssertEqual(Solution().lemonadeChange(input), output)
+    }
+
+    func testLemonadeChange2() {
+        let input = [5, 5, 10]
+        let output = true
+        XCTAssertEqual(Solution().lemonadeChange(input), output)
+    }
+
+    func testLemonadeChange3() {
+        let input = [10, 10]
+        let output = false
+        XCTAssertEqual(Solution().lemonadeChange(input), output)
+    }
+
+    func testLemonadeChange4() {
+        let input = [5, 5, 10, 10, 20]
+        let output = false
+        XCTAssertEqual(Solution().lemonadeChange(input), output)
+    }
+
+    func testLemonadeChange5() {
+        let input = [5, 5, 10, 20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10, 5, 5, 20, 5, 20, 5]
+        let output = true
+        XCTAssertEqual(Solution().lemonadeChange(input), output)
+    }
+}
