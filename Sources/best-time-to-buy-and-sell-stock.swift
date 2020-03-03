af4e7a0a5d6cfc4f35c0689a2eb1c4ba4f8a53fa
mttcrsp <crespi.matteo@icloud.com>
2020-03-03 23:40:05 +0100

    121. Best Time to Buy and Sell Stock

---
+++ Sources/best-time-to-buy-and-sell-stock.swift
@@ -0,0 +1,20 @@
+extension Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        guard let first = prices.first else {
+            return 0
+        }
+
+        var difference = 0
+        var min = first
+
+        for price in prices.dropFirst() {
+            if price < min {
+                min = price
+            } else if price - min > difference {
+                difference = price - min
+            }
+        }
+
+        return difference
+    }
+}
---
+++ Tests/best-time-to-buy-and-sell-stock-tests.swift
@@ -0,0 +1,29 @@
+@testable
+import Leetcode
+import XCTest
+
+extension SolutionTests {
+    func testMaxProfit1() {
+        let input = [7, 1, 5, 3, 6, 4]
+        let output = 5
+        XCTAssertEqual(Solution().maxProfit(input), output)
+    }
+
+    func testMaxProfit2() {
+        let input = [7, 6, 4, 3, 1]
+        let output = 0
+        XCTAssertEqual(Solution().maxProfit(input), output)
+    }
+
+    func testMaxProfit3() {
+        let input = [10, 7, 5, 2, 7, 11, 1, 2, 5, 7]
+        let output = 9
+        XCTAssertEqual(Solution().maxProfit(input), output)
+    }
+
+    func testMaxProfit4() {
+        let input = [] as [Int]
+        let output = 0
+        XCTAssertEqual(Solution().maxProfit(input), output)
+    }
+}
