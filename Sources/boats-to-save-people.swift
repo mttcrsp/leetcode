dc4e973055a9ac61ff195c6399a143d87459bf7a
mttcrsp <crespi.matteo@icloud.com>
2020-03-09 21:48:51 +0100

    881. Boats to Save People

---
+++ Sources/boats-to-save-people.swift
@@ -0,0 +1,42 @@
+extension Solution {
+    func numRescueBoats(_ people: [Int], _ limit: Int) -> Int {
+        assert(people.allSatisfy { weight in weight <= limit })
+
+        var countsForWeight: [Int: Int] = [:]
+        for weight in people {
+            countsForWeight[weight, default: 0] += 1
+        }
+
+        var numberOfBoats = 0
+        var sortedCountsForWeight = countsForWeight.sorted { lhs, rhs in
+            lhs.key < rhs.key
+        }
+
+        while let (fatWeight, fatCount) = sortedCountsForWeight.last,
+            let (thinWeight, thinCount) = sortedCountsForWeight.first {
+            if fatWeight + thinWeight > limit {
+                numberOfBoats += fatCount
+                sortedCountsForWeight.removeLast()
+            } else if thinCount > fatCount {
+                numberOfBoats += fatCount
+                sortedCountsForWeight[0].value -= fatCount
+                sortedCountsForWeight.removeLast()
+            } else if fatCount > thinCount {
+                numberOfBoats += thinCount
+                sortedCountsForWeight[sortedCountsForWeight.count - 1].value -= thinCount
+                sortedCountsForWeight.removeFirst()
+            } else if thinCount == fatCount {
+                if thinWeight == fatWeight {
+                    numberOfBoats += (fatCount / 2) + (fatCount % 2)
+                    sortedCountsForWeight.removeLast()
+                } else {
+                    numberOfBoats += fatCount
+                    sortedCountsForWeight.removeFirst()
+                    sortedCountsForWeight.removeLast()
+                }
+            }
+        }
+
+        return numberOfBoats
+    }
+}
---
+++ Tests/boats-to-save-people-tests.swift
@@ -0,0 +1,47 @@
+@testable
+import Leetcode
+import XCTest
+
+extension SolutionTests {
+    func testNumRescueBoats1() {
+        let input = ([1, 2], 3)
+        let output = 1
+        XCTAssertEqual(Solution().numRescueBoats(input.0, input.1), output)
+    }
+
+    func testNumRescueBoats2() {
+        let input = ([3, 2, 2, 1], 3)
+        let output = 3
+        XCTAssertEqual(Solution().numRescueBoats(input.0, input.1), output)
+    }
+
+    func testNumRescueBoats3() {
+        let input = ([3, 5, 3, 4], 5)
+        let output = 4
+        XCTAssertEqual(Solution().numRescueBoats(input.0, input.1), output)
+    }
+
+    func testNumRescueBoats4() {
+        let input = ([] as [Int], 5)
+        let output = 0
+        XCTAssertEqual(Solution().numRescueBoats(input.0, input.1), output)
+    }
+
+    func testNumRescueBoats5() {
+        let input = ([5, 5, 5, 5, 5], 5)
+        let output = 5
+        XCTAssertEqual(Solution().numRescueBoats(input.0, input.1), output)
+    }
+
+    func testNumRescueBoats6() {
+        let input = ([2, 2, 2, 1], 4)
+        let output = 2
+        XCTAssertEqual(Solution().numRescueBoats(input.0, input.1), output)
+    }
+
+    func testNumRescueBoats7() {
+        let input = ([8, 2, 3, 6, 2, 6], 8)
+        let output = 4
+        XCTAssertEqual(Solution().numRescueBoats(input.0, input.1), output)
+    }
+}
