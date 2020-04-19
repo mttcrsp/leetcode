09a2bbe832ae52b6d4c0bddc0dbcc01be72643b5
mttcrsp <crespi.matteo@icloud.com>
2020-04-20 00:26:54 +0200

    594. Longest Harmonious Subsequence

---
+++ Tests/longest-harmonious-subsequence-tests.swift
@@ -0,0 +1,11 @@
+@testable
+import Leetcode
+import XCTest
+
+extension SolutionTests {
+    func testFindLHS1() {
+        let input = [1, 3, 2, 2, 5, 2, 3, 7]
+        let output = 5
+        XCTAssertEqual(Solution().findLHS(input), output)
+    }
+}
