c0ebee119869a8c915bdc367ba722d70adc0a612
Matteo Crespi <crespi.matteo@icloud.com>
2020-07-01 23:13:43 +0200

    344. Reverse String

---
+++ Tests/reverse-string-tests.swift
@@ -0,0 +1,21 @@
+@testable
+import Leetcode
+import XCTest
+
+extension SolutionTests {
+    func testReverseString1() {
+        var input: [Character] = ["h", "e", "l", "l", "o"]
+        Solution().reverseString(&input)
+
+        let output: [Character] = ["o", "l", "l", "e", "h"]
+        XCTAssertEqual(input, output)
+    }
+
+    func testReverseString2() {
+        var input: [Character] = ["H", "a", "n", "n", "a", "h"]
+        Solution().reverseString(&input)
+
+        let output: [Character] = ["h", "a", "n", "n", "a", "H"]
+        XCTAssertEqual(input, output)
+    }
+}
