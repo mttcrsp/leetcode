d7c5c881523ae11ac98ebf6a6e03542abeadaef0
Matteo Crespi <crespi.matteo@icloud.com>
2020-07-01 23:06:16 +0200

    1022. Sum of Root To Leaf Binary Numbers

---
+++ Sources/sum-of-root-to-leaf-binary-numbers.swift
@@ -0,0 +1,16 @@
+extension Solution {
+    func sumRootToLeaf(_ root: TreeNode?) -> Int {
+        var result = 0
+
+        func visit(_ node: TreeNode, withContext context: Int) {
+            let context = (context << 1) + node.val
+            if let lhs = node.left { visit(lhs, withContext: context) }
+            if let rhs = node.right { visit(rhs, withContext: context) }
+            if node.left == nil, node.right == nil { result += context }
+        }
+
+        if let node = root { visit(node, withContext: 0) }
+
+        return result
+    }
+}
