ac517510c665f5f7b4c832dbc39ef41ce2c788c2
Matteo Crespi <crespi.matteo@icloud.com>
2020-07-03 22:49:38 +0200

    283. Move Zeroes (cont)

--- Sources/move-zeroes.swift
+++ Sources/move-zeroes.swift
@@ -1,18 +1,10 @@
 extension Solution {
     func moveZeroes(_ numbers: inout [Int]) {
-        var valid = 0
-        var zeros = 0
-        while valid + zeros < numbers.count {
-            if numbers[valid] == 0 {
-                var i = valid
-                while i < numbers.count - 1 - zeros {
-                    numbers.swapAt(i, i + 1)
-                    i += 1
-                }
-
-                zeros += 1
-            } else {
-                valid += 1
+        var j = 0
+        for i in numbers.indices {
+            if numbers[i] != 0 {
+                numbers.swapAt(i, j)
+                j += 1
             }
         }
     }
