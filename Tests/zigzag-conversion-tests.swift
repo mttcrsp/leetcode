c87c588780aee633f896ed8dbe8398c7c147e2b5
Matteo Crespi <crespi.matteo@icloud.com>
2020-07-11 01:44:21 +0200

    6. ZigZag Conversion

---
+++ Tests/zigzag-conversion-tests.swift
@@ -0,0 +1,29 @@
+@testable
+import Leetcode
+import XCTest
+
+extension SolutionTests {
+    func testConvert1() {
+        let input = ("PAYPALISHIRING", 0)
+        let output = "PAYPALISHIRING"
+        XCTAssertEqual(Solution().convert(input.0, input.1), output)
+    }
+
+    func testConvert2() {
+        let input = ("PAYPALISHIRING", 1)
+        let output = "PAYPALISHIRING"
+        XCTAssertEqual(Solution().convert(input.0, input.1), output)
+    }
+
+    func testConvert() {
+        let input = ("PAYPALISHIRING", 3)
+        let output = "PAHNAPLSIIGYIR"
+        XCTAssertEqual(Solution().convert(input.0, input.1), output)
+    }
+
+    func testConvert4() {
+        let input = ("PAYPALISHIRING", 4)
+        let output = "PINALSIGYAHRPI"
+        XCTAssertEqual(Solution().convert(input.0, input.1), output)
+    }
+}
