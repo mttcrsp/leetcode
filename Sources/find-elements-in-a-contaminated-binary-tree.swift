c3616ef02b07db1d47331ecd56dda69686b06ea5
Matteo Crespi <crespi.matteo@icloud.com>
2020-07-11 16:00:32 +0200

    1261. Find Elements in a Contaminated Binary Tree

---
+++ Sources/find-elements-in-a-contaminated-binary-tree.swift
@@ -0,0 +1,32 @@
+class FindElements {
+    private var values: Set<Int> = []
+
+    init(_ root: TreeNode?) {
+        func visit(_ node: TreeNode) {
+            let x = node.val
+
+            if let left = node.left {
+                let value = 2 * x + 1
+                left.val = value
+                values.insert(value)
+                visit(left)
+            }
+
+            if let right = node.right {
+                let value = 2 * x + 2
+                right.val = value
+                values.insert(value)
+                visit(right)
+            }
+        }
+
+        if let node = root {
+            node.val = 0
+            visit(node)
+        }
+    }
+
+    func find(_ target: Int) -> Bool {
+        values.contains(target)
+    }
+}
