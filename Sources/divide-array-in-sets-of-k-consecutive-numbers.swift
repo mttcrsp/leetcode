d5f0378cd5fc846a89a35a0de16150b0b9f0421d
Matteo Crespi <crespi.matteo@icloud.com>
2020-07-11 15:50:30 +0200

    1296. Divide Array in Sets of K Consecutive Numbers

---
+++ Sources/divide-array-in-sets-of-k-consecutive-numbers.swift
@@ -0,0 +1,19 @@
+extension Solution {
+    func isPossibleDivide(_ nums: [Int], _ k: Int) -> Bool {
+        var counts: [Int: Int] = [:]
+        for num in nums {
+            counts[num, default: 0] += 1
+        }
+
+        for key in counts.keys.sorted() {
+            while counts[key, default: 0] > 0 {
+                for i in 0 ..< k {
+                    let updated = counts[key + i, default: 0] - 1
+                    counts[key + i] = updated == 0 ? nil : updated
+                }
+            }
+        }
+
+        return counts.isEmpty
+    }
+}
