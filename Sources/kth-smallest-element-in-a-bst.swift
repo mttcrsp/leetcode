9186b77682d2412c909c510cefe4282fbd2cdab4
Matteo Crespi <crespi.matteo@icloud.com>
2020-07-12 17:10:01 +0200

    230. Kth Smallest Element in a BST

---
+++ Sources/kth-smallest-element-in-a-bst.swift
@@ -0,0 +1,22 @@
+extension Solution {
+    func kthSmallest(_ root: TreeNode?, _ k: Int) -> Int {
+        var count = 0
+        var result: Int?
+
+        func visit(_ node: TreeNode?) {
+            if let node = node, count < k {
+                visit(node.left)
+                count += 1
+                if count == k {
+                    result = node.val
+                }
+                visit(node.right)
+            }
+        }
+
+        visit(root)
+
+        precondition(result != nil, "Input BTS contains less that k elements")
+        return result!
+    }
+}
