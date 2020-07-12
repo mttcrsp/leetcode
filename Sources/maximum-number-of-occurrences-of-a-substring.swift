ef44bea473cec28bbb2a9991e244a5483ef2e822
Matteo Crespi <crespi.matteo@icloud.com>
2020-07-12 11:32:29 +0200

    1297. Maximum Number of Occurrences of a Substring

---
+++ Sources/maximum-number-of-occurrences-of-a-substring.swift
@@ -0,0 +1,24 @@
+extension Solution {
+    func maxFreq(_ s: String, _ maxLetters: Int, _ minSize: Int, _: Int) -> Int {
+        let string = Array(s)
+        var letters: [Character: Int] = [:]
+        var occurrences: [ArraySlice<Character>: Int] = [:]
+
+        for upperbound in string.indices {
+            letters[string[upperbound], default: 0] += 1
+
+            let lowerbound = upperbound - minSize + 1
+            guard lowerbound >= 0 else { continue }
+
+            if letters.count <= maxLetters {
+                let substring = string[lowerbound ... upperbound]
+                occurrences[substring, default: 0] += 1
+            }
+
+            let updated = letters[string[lowerbound], default: 0] - 1
+            letters[string[lowerbound]] = updated == 0 ? nil : updated
+        }
+
+        return occurrences.values.max() ?? 0
+    }
+}
