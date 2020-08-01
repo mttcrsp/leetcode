7acf4692e4cdcf16799b527b35f080a2b349e7e5
Matteo Crespi <crespi.matteo@icloud.com>
2020-08-01 18:37:46 +0200

    125. Valid Palindrome

---
+++ Tests/valid-palindrome-tests.swift
@@ -0,0 +1,23 @@
+@testable
+import Leetcode
+import XCTest
+
+extension SolutionTests {
+    func testIsPalindrome1() {
+        let input = "A man, a plan, a canal: Panama"
+        let output = true
+        XCTAssertEqual(Solution().isPalindrome(input), output)
+    }
+
+    func testIsPalindrome2() {
+        let input = "race a car"
+        let output = false
+        XCTAssertEqual(Solution().isPalindrome(input), output)
+    }
+
+    func testIsPalindrome3() {
+        let input = "0A"
+        let output = false
+        XCTAssertEqual(Solution().isPalindrome(input), output)
+    }
+}
