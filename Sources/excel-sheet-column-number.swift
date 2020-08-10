1e0a119d4a322f04ba05af87527c28a564a1d624
Matteo Crespi <crespi.matteo@icloud.com>
2020-08-10 22:09:34 +0200

    171. Excel Sheet Column Number (cont)

--- Sources/excel-sheet-column-number.swift
+++ Sources/excel-sheet-column-number.swift
@@ -1,24 +1,29 @@
 import Foundation
 
 extension Solution {
-    func titleToNumber(_ s: String) -> Int {
-        var number = 0
-        for (i, character) in s.reversed().enumerated() {
-            let base = Double(26), exponent = Double(i)
-            let value = character.alphabeticalValue
-            number += value * Int(pow(base, exponent))
+    func titleToNumber(_ string: String) -> Int {
+        let base: Double = Character.lastExcelCharacter.excelValue
+
+        var result: Double = 0
+
+        for (offset, character) in string.reversed().enumerated() {
+            result += pow(base, Double(offset)) * character.excelValue
         }
-        return number
+
+        return Int(result)
     }
 }
 
 private extension Character {
-    var alphabeticalValue: Int {
-        if let current = asciiValue, let reference = Character("A").asciiValue {
-            return Int(current - reference + 1)
-        } else {
-            assertionFailure()
-            return 0
-        }
+    var excelValue: Double {
+        Double(asciiValue! - Character.firstExcelCharacter.asciiValue!) + 1
+    }
+
+    static var firstExcelCharacter: Character {
+        "A"
+    }
+
+    static var lastExcelCharacter: Character {
+        "Z"
     }
 }
