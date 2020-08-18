9bbfc20d89e78d62ea48ad781d4868265776f59e
Matteo Crespi <crespi.matteo@icloud.com>
2020-08-19 01:11:11 +0200

    967. Numbers With Same Consecutive Differences

---
+++ Sources/numbers-with-same-consecutive-differences.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+extension Solution {
+    func numsSameConsecDiff(_ count: Int, _ difference: Int) -> [Int] {
+        if count == 1 {
+            return Array(0 ... 9)
+        }
+
+        var result: [Int] = []
+
+        func visit(_ count: Int, _ values: [Int]) {
+            if count == 0 {
+                return result.append(values.numberValue)
+            }
+
+            for value in 0 ... 9 where abs(Double(values.last!) - Double(value)) == Double(difference) {
+                visit(count - 1, values + [value])
+            }
+        }
+
+        for i in 1 ... 9 {
+            visit(count - 1, [i])
+        }
+
+        return result
+    }
+}
+
+extension Array where Element == Int {
+    var numberValue: Int {
+        var factor = 1
+        var result = 0
+
+        for index in indices.reversed() {
+            result += factor * self[index]
+            factor *= 10
+        }
+
+        return result
+    }
+}
---
+++ Tests/numbers-with-same-consecutive-differences-tests.swift
@@ -0,0 +1,29 @@
+@testable
+import Leetcode
+import XCTest
+
+extension SolutionTests {
+    func testNumsSameConsecDiff1() {
+        let input = (1, 1)
+        let output = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        XCTAssertEqual(Solution().numsSameConsecDiff(input.0, input.1), output)
+    }
+
+    func testNumsSameConsecDiff2() {
+        let input = (3, 7)
+        let output = [181, 292, 707, 818, 929]
+        XCTAssertEqual(Solution().numsSameConsecDiff(input.0, input.1), output)
+    }
+
+    func testNumsSameConsecDiff3() {
+        let input = (2, 1)
+        let output = [10, 12, 21, 23, 32, 34, 43, 45, 54, 56, 65, 67, 76, 78, 87, 89, 98]
+        XCTAssertEqual(Solution().numsSameConsecDiff(input.0, input.1), output)
+    }
+
+    func testNumsSameConsecDiff4() {
+        let input = (4, 5)
+        let output = [1616, 2727, 3838, 4949, 5050, 6161, 7272, 8383, 9494]
+        XCTAssertEqual(Solution().numsSameConsecDiff(input.0, input.1), output)
+    }
+}
