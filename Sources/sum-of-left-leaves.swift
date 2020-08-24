ab700e06f7311ccea2079cb3ef325b7ecfdb8e44
mttcrsp <crespi.matteo@icloud.com>
2020-08-24 16:00:27 +0200

    404. Sum of Left Leaves (cont)

--- Sources/sum-of-left-leaves.swift
+++ Sources/sum-of-left-leaves.swift
@@ -1,18 +1,27 @@
 extension Solution {
     func sumOfLeftLeaves(_ root: TreeNode?) -> Int {
-        return root?.visit(isLeft: false) ?? 0
-    }
-}
+        guard let root = root else { return 0 }
 
-private extension TreeNode {
-    func visit(isLeft: Bool) -> Int {
-        if left == nil, right == nil {
-            return isLeft ? val : 0
+        var result = 0
+        var stack: [(TreeNode, Bool)] = [(root, false)]
+
+        while !stack.isEmpty {
+            let (node, isLeft) = stack.removeLast()
+
+            if node.left == nil, node.right == nil {
+                if isLeft {
+                    result += node.val
+                }
+            } else {
+                if let left = node.left {
+                    stack.append((left, true))
+                }
+                if let right = node.right {
+                    stack.append((right, false))
+                }
+            }
         }
-        
-        var count = 0
-        count += left?.visit(isLeft: true) ?? 0
-        count += right?.visit(isLeft: false) ?? 0
-        return count
+
+        return result
     }
 }
