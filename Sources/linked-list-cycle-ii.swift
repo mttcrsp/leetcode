36fb94822c3a100eed28603333e87a94c8a2e0d2
mttcrsp <crespi.matteo@icloud.com>
2020-08-29 18:12:51 +0200

    142. Linked List Cycle II

---
+++ Sources/linked-list-cycle-ii.swift
@@ -0,0 +1,19 @@
+extension Solution {
+    func detectCycle(_ head: ListNode?) -> ListNode? {
+        var addresses: Set<Int> = []
+        var current = head
+
+        while let node = current {
+            let address = unsafeBitCast(node, to: Int.self)
+            let (didInsert, _) = addresses.insert(address)
+
+            if didInsert {
+                current = node.next
+            } else {
+                return node
+            }
+        }
+
+        return nil
+    }
+}
