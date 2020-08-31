7b7ee5b89659501ba497bfa2be9179eda18db878
mttcrsp <crespi.matteo@icloud.com>
2020-08-31 23:55:14 +0200

    450. Delete Node in a BST

---
+++ Sources/delete-node-in-a-bst.swift
@@ -0,0 +1,29 @@
+extension Solution {
+    func deleteNode(_ node: TreeNode?, _ key: Int) -> TreeNode? {
+        var sorted: [TreeNode] = []
+
+        func visit(_ node: TreeNode?) {
+            if let node = node {
+                visit(node.left)
+                sorted.append(node)
+                visit(node.right)
+            }
+        }
+
+        visit(node)
+
+        sorted.removeAll { node in
+            node.val == key
+        }
+
+        for i in sorted.indices.dropLast() {
+            sorted.last?.left = nil
+            sorted[i].right = sorted[i + 1]
+        }
+
+        sorted.last?.left = nil
+        sorted.last?.right = nil
+
+        return sorted.first
+    }
+}
