01f53ed0633817c8479645031d86995bffcfc6a8
mttcrsp <crespi.matteo@icloud.com>
2020-09-05 23:19:15 +0200

    1305. All Elements in Two Binary Search Trees

---
+++ Sources/all-elements-in-two-binary-search-trees.swift
@@ -0,0 +1,22 @@
+extension Solution {
+  func getAllElements(_ root1: TreeNode?, _ root2: TreeNode?) -> [Int] {
+    var halfSorted: [Int] = []
+
+    func visit(_ node: TreeNode?) {
+      if let node = node {
+        visit(node.left)
+        halfSorted.append(node.val)
+        visit(node.right)
+      }
+    }
+
+    visit(root1)
+    visit(root2)
+
+    // This solution runs in O(n log n) due to this sorting. This can be easily
+    // made linear either by applying radix sort or by using the merge algorithm
+    // from merge sort. Both procedures run in O(n) and will reduce the time
+    // complexity of the overall algorigthm to O(n).
+    return halfSorted.sorted()
+  }
+}
