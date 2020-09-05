0de9937b6f7456ba8c13159353318a21788b311e
mttcrsp <crespi.matteo@icloud.com>
2020-09-05 22:56:53 +0200

    1356. Sort Integers by The Number of 1 Bits

---
+++ Sources/sort-integers-by-the-number-of-1-bits.swift
@@ -0,0 +1,11 @@
+extension Solution {
+  func sortByBits(_ numbers: [Int]) -> [Int] {
+    numbers.sorted { lhs, rhs in
+      if lhs.nonzeroBitCount == rhs.nonzeroBitCount {
+        return lhs < rhs
+      } else {
+        return lhs.nonzeroBitCount < rhs.nonzeroBitCount
+      }
+    }
+  }
+}
---
+++ Tests/sort-integers-by-the-number-of-1-bits-tests.swift
@@ -0,0 +1,17 @@
+@testable
+import Leetcode
+import XCTest
+
+extension SolutionTests {
+  func testSortByBits1() {
+    let input = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    let output = [0, 1, 2, 4, 8, 3, 5, 6, 7]
+    XCTAssertEqual(Solution().sortByBits(input), output)
+  }
+
+  func testSortByBits2() {
+    let input = [1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1]
+    let output = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+    XCTAssertEqual(Solution().sortByBits(input), output)
+  }
+}
