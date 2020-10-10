0425c3a770814865309462ed2ac03b6b32d38fb9
mttcrsp <crespi.matteo@icloud.com>
2020-10-10 22:46:10 +0200

    540. Single Element in a Sorted Array

---
+++ Sources/single-element-in-a-sorted-array.swift
@@ -0,0 +1,5 @@
+extension Solution {
+  func singleNonDuplicate(_ numbers: [Int]) -> Int {
+    numbers.reduce(0, ^)
+  }
+}
---
+++ Tests/single-element-in-a-sorted-array-tests.swift
@@ -0,0 +1,17 @@
+@testable
+import Leetcode
+import XCTest
+
+extension SolutionTests {
+  func testSingleNonDuplicate1() {
+    let input = [1, 1, 2, 3, 3, 4, 4, 8, 8]
+    let output = 2
+    XCTAssertEqual(Solution().singleNonDuplicate(input), output)
+  }
+
+  func testSingleNonDuplicate2() {
+    let input = [3, 3, 7, 7, 10, 11, 11]
+    let output = 10
+    XCTAssertEqual(Solution().singleNonDuplicate(input), output)
+  }
+}
