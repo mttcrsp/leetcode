9f818a930b0d9998fa72da245fc7c559c4502a8d
mttcrsp <crespi.matteo@icloud.com>
2020-10-10 23:24:21 +0200

    1288. Remove Covered Intervals

---
+++ Sources/remove-covered-intervals.swift
@@ -0,0 +1,23 @@
+extension Solution {
+  func removeCoveredIntervals(_ intervals: [[Int]]) -> Int {
+    let sortedIntervals = intervals.sorted { lhs, rhs in
+      lhs[0] < rhs[0]
+    }
+
+    var lhs = -1, rhs = -1, count = 0
+    for interval in sortedIntervals {
+      if interval.lhs > lhs, interval.rhs > rhs {
+        lhs = interval.lhs
+        count += 1
+      }
+      rhs = max(interval.rhs, rhs)
+    }
+
+    return count
+  }
+}
+
+private extension Array where Element == Int {
+  var lhs: Int { self[0] }
+  var rhs: Int { self[1] }
+}
