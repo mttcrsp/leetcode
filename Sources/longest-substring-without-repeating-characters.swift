c855d0312da110b24023f092177ee3e4a8a27722
mttcrsp <crespi.matteo@icloud.com>
2020-10-11 17:26:21 +0200

    3. Longest Substring Without Repeating Characters

---
+++ Sources/longest-substring-without-repeating-characters.swift
@@ -0,0 +1,17 @@
+extension Solution {
+  func lengthOfLongestSubstring(_ s: String) -> Int {
+    let characters = Array(s)
+
+    var max = 0
+    var rhs = 0
+
+    for lhs in characters.indices {
+      while rhs < characters.count, Set(characters[lhs ... rhs]).count >= rhs - lhs + 1 {
+        max = Swift.max(max, rhs - lhs + 1)
+        rhs += 1
+      }
+    }
+
+    return max
+  }
+}
