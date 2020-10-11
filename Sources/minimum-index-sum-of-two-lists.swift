30d1e4fdaa4c9ef35dc904e50b89db4103754df9
mttcrsp <crespi.matteo@icloud.com>
2020-10-11 16:53:43 +0200

    599. Minimum Index Sum of Two Lists (cont)

--- Sources/minimum-index-sum-of-two-lists.swift
+++ Sources/minimum-index-sum-of-two-lists.swift
@@ -1,36 +1,28 @@
 extension Solution {
   func findRestaurant(_ list1: [String], _ list2: [String]) -> [String] {
-    let enumeratedDictionary1 = list1.enumeratedDictionary()
-    let enumeratedDictionary2 = list2.enumeratedDictionary()
-    let commonRestaurants = Set(list1).intersection(list2)
+    var indices: [String: Int] = [:]
+    for (index, restaurant) in list1.enumerated() {
+      indices[restaurant] = index
+    }
 
     var min: (indexSum: Int, restaurants: [String])?
-    for restaurant in commonRestaurants {
-      let index1 = enumeratedDictionary1[restaurant]!
-      let index2 = enumeratedDictionary2[restaurant]!
-      let newIndexSum = index1 + index2
+    for (index2, restaurant) in list2.enumerated() {
+      guard let index1 = indices[restaurant] else {
+        continue
+      }
 
-      if let (indexSum, restaurants) = min {
-        if newIndexSum < indexSum {
-          min = (newIndexSum, [restaurant])
-        } else if newIndexSum == indexSum {
-          min = (newIndexSum, [restaurant] + restaurants)
-        }
-      } else {
+      let newIndexSum = index1 + index2
+      guard let (indexSum, restaurants) = min else {
+        min = (newIndexSum, [restaurant]); continue
+      }
+
+      if newIndexSum < indexSum {
         min = (newIndexSum, [restaurant])
+      } else if newIndexSum == indexSum {
+        min = (newIndexSum, [restaurant] + restaurants)
       }
     }
 
     return min?.restaurants ?? []
-  }
-}
-
-private extension Array where Element: Hashable {
-  func enumeratedDictionary() -> [Element: Int] {
-    var dictionary: [Element: Int] = [:]
-    for (index, element) in enumerated() {
-      dictionary[element] = index
-    }
-    return dictionary
   }
 }
