e88207bbca23ccf3d79f593e6ea578fcc49c12c5
mttcrsp <crespi.matteo@icloud.com>
2020-11-14 17:00:03 +0100

    589. N-ary Tree Preorder Traversal

---
+++ Sources/NaryTreeNode.swift
@@ -0,0 +1,9 @@
+class NaryTreeNode {
+  var val: Int
+  var children: [NaryTreeNode]
+
+  init(_ val: Int) {
+    self.val = val
+    children = []
+  }
+}
---
+++ Sources/n-ary-tree-preorder-traversal.swift
@@ -0,0 +1,19 @@
+extension Solution {
+  func preorder(_ root: NaryTreeNode?) -> [Int] {
+    var result: [Int] = []
+
+    func visit(_ node: NaryTreeNode?) {
+      guard let node = node else { return }
+
+      result.append(node.val)
+
+      for child in node.children {
+        visit(child)
+      }
+    }
+
+    visit(root)
+
+    return result
+  }
+}
