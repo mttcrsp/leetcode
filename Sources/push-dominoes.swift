6683b876dd42c9a600be922d29c2fdf3ba7409d7
mttcrsp <crespi.matteo@icloud.com>
2021-01-17 01:13:39 +0100

    838. Push Dominoes (cont)

--- Sources/push-dominoes.swift
+++ Sources/push-dominoes.swift
@@ -1,31 +1,45 @@
 extension Solution {
+  private struct Push {
+    let direction: Character
+    let index: Int
+  }
+
   func pushDominoes(_ initialStates: String) -> String {
     var states = [Character](initialStates)
 
-    while true {
-      var proposals: [Int: Character] = [:]
-
-      for (i, state) in states.enumerated() {
-        let prev = i - 1
-        let next = i + 1
-
-        if state == "R", next < states.count, states[next] == "." {
-          proposals[next] = "R"
-        } else if state == "L", prev >= 0, states[prev] == "." {
-          if proposals[prev] != nil {
-            proposals[prev] = nil
-          } else {
-            proposals[prev] = "L"
-          }
-        }
+    func setState(_ character: Character, in range: Range<Int>) {
+      for i in range {
+        states[i] = character
       }
+    }
 
-      for (i, state) in proposals {
-        states[i] = state
-      }
+    var pushes = states.enumerated().compactMap { i, direction in
+      direction == "." ? nil : Push(direction: direction, index: i)
+    }
 
-      if proposals.isEmpty {
-        break
+    while pushes.first?.direction == "L" {
+      let push = pushes.removeFirst()
+      setState("L", in: 0 ..< push.index)
+    }
+
+    while pushes.last?.direction == "R" {
+      let push = pushes.removeLast()
+      setState("R", in: push.index + 1 ..< states.count)
+    }
+
+    for i in pushes.indices.dropLast() {
+      let lhs = pushes[i]
+      let rhs = pushes[i + 1]
+
+      switch (lhs.direction, rhs.direction) {
+      case ("L", "L"), ("R", "R"):
+        setState(lhs.direction, in: lhs.index + 1 ..< rhs.index)
+      case ("R", "L"):
+        let distance = (rhs.index - lhs.index - 1) / 2
+        setState("R", in: lhs.index + 1 ..< lhs.index + distance + 1)
+        setState("L", in: rhs.index - distance ..< rhs.index)
+      default:
+        continue
       }
     }
 
