6b55653b726f53aed9f6cc9e4139d0abfa3123c7
Matteo Crespi <crespi.matteo@icloud.com>
2021-03-14 17:35:59 +0100

    1630. Arithmetic Subarrays (cont)

--- Sources/arithmetic-subarrays.swift
+++ Sources/arithmetic-subarrays.swift
@@ -1,30 +1,28 @@
 struct ArithmeticSubarrays {
   func checkArithmeticSubarrays(_ numbers: [Int], _ lowerbounds: [Int], _ upperbounds: [Int]) -> [Bool] {
-    var result: [Bool] = []
-    for (lowerbound, upperbound) in zip(lowerbounds, upperbounds) {
-      if upperbound - lowerbound < 2 {
-        result.append(true)
-        continue
+    zip(lowerbounds, upperbounds).map { lowerbound, upperbound in
+      let range = numbers[lowerbound ... upperbound]
+
+      var min: Int = .max
+      var max: Int = .min
+      var numbers: Set<Int> = []
+      for number in range {
+        min = Swift.min(min, number)
+        max = Swift.max(max, number)
+        numbers.insert(number)
       }
 
-      let sortedRange = numbers[lowerbound ... upperbound].sorted()
-      let consecutives = zip(sortedRange, sortedRange.dropFirst())
-
-      var distance: Int?, valid = true
-      loop: for (lhs, rhs) in consecutives {
-        switch distance {
-        case nil:
-          distance = rhs - lhs
-        case let .some(distance) where distance == rhs - lhs:
-          continue
-        case .some:
-          valid = false
-          break loop
-        }
+      if numbers.count == 1 {
+        return true
       }
-      result.append(valid)
+
+      guard (max - min) % (range.count - 1) == 0 else {
+        return false
+      }
+
+      let distance = (max - min) / (range.count - 1)
+      let expectedNumbers = stride(from: min, to: max, by: distance)
+      return expectedNumbers.allSatisfy(numbers.contains)
     }
-
-    return result
   }
 }
--- Tests/arithmetic-subarrays-tests.swift
+++ Tests/arithmetic-subarrays-tests.swift
@@ -4,13 +4,21 @@ import XCTest
 
 final class ArithmeticSubarraysTests: XCTestCase {
   func testCheckArithmeticSubarrays1() {
-    let input = ([4, 6, 5, 9, 3, 7], [0, 0, 2], [2, 3, 5])
+    let input = (
+      [4, 6, 5, 9, 3, 7],
+      [0, 0, 2],
+      [2, 3, 5]
+    )
     let output = [true, false, true]
     XCTAssertEqual(ArithmeticSubarrays().checkArithmeticSubarrays(input.0, input.1, input.2), output)
   }
 
   func testCheckArithmeticSubarrays2() {
-    let input = ([-12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10], [0, 1, 6, 4, 8, 7], [4, 4, 9, 7, 9, 10])
+    let input = (
+      [-12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10],
+      [0, 1, 6, 4, 8, 7],
+      [4, 4, 9, 7, 9, 10]
+    )
     let output = [false, true, false, false, true, true]
     XCTAssertEqual(ArithmeticSubarrays().checkArithmeticSubarrays(input.0, input.1, input.2), output)
   }
