2d1392a8203c81d3d0a586321cffa1c5112ddc16
Matteo Crespi <crespi.matteo@icloud.com>
2021-03-22 01:12:43 +0100

    938. Range Sum of BST

---
+++ Sources/range-sum-of-bst.swift
@@ -0,0 +1,22 @@
+struct RangeSumOfBst {
+  func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> Int {
+    guard let root = root else { return 0 }
+
+    var sum = 0
+    var unvisited = [root]
+    while !unvisited.isEmpty {
+      let node = unvisited.removeLast()
+      if node.val >= low, node.val <= high {
+        sum += node.val
+      }
+      if let left = node.left {
+        unvisited.append(left)
+      }
+      if let right = node.right {
+        unvisited.append(right)
+      }
+    }
+
+    return sum
+  }
+}
