4eb97e3efd48009b15ef02e8f31159ba0c261252
Matteo Crespi <crespi.matteo@icloud.com>
2021-04-04 00:21:17 +0200

    173. Binary Search Tree Iterator

---
+++ Sources/binary-search-tree-iterator.swift
@@ -0,0 +1,25 @@
+class BSTIterator {
+  private var array: [Int] = []
+
+  init(_ root: TreeNode?) {
+    func visit(_ node: TreeNode?) {
+      if let node = node {
+        visit(node.left)
+        array.append(node.val)
+        visit(node.right)
+      }
+    }
+
+    visit(root)
+
+    array.reverse()
+  }
+
+  func next() -> Int {
+    array.removeLast()
+  }
+
+  func hasNext() -> Bool {
+    !array.isEmpty
+  }
+}
---
+++ Tests/binary-search-tree-iterator-tests.swift
@@ -0,0 +1,18 @@
+@testable
+import Leetcode
+import XCTest
+
+final class BinarySearchTreeIteratorTests: XCTestCase {
+  func testBstIterator1() {
+    let iterator = BSTIterator(TreeNode.makeTree(with: [7, 3, 15, nil, nil, 9, 20]))
+    XCTAssertEqual(iterator.next(), 3)
+    XCTAssertEqual(iterator.next(), 7)
+    XCTAssertTrue(iterator.hasNext())
+    XCTAssertEqual(iterator.next(), 9)
+    XCTAssertTrue(iterator.hasNext())
+    XCTAssertEqual(iterator.next(), 15)
+    XCTAssertTrue(iterator.hasNext())
+    XCTAssertEqual(iterator.next(), 20)
+    XCTAssertFalse(iterator.hasNext())
+  }
+}
