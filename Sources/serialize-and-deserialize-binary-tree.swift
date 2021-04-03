16b18adb2e960d77856e68e380ab9618859cc3b3
Matteo Crespi <crespi.matteo@icloud.com>
2021-04-04 00:12:00 +0200

    297. Serialize and Deserialize Binary Tree

---
+++ Sources/serialize-and-deserialize-binary-tree.swift
@@ -0,0 +1,39 @@
+class Codec {
+  func serialize(_ root: TreeNode?) -> String {
+    var values: [String] = []
+
+    func visit(_ node: TreeNode?) {
+      guard let node = node else {
+        return values.append("n")
+      }
+
+      values.append(node.val.description)
+      visit(node.left)
+      visit(node.right)
+    }
+
+    visit(root)
+
+    return values.joined(separator: ",")
+  }
+
+  func deserialize(_ serialized: String) -> TreeNode? {
+    let values = serialized.components(separatedBy: ",").map(Int.init)
+    var index = 0
+
+    func visit() -> TreeNode? {
+      guard let value = values[index] else {
+        index += 1
+        return nil
+      }
+
+      index += 1
+      let node = TreeNode(value)
+      node.left = visit()
+      node.right = visit()
+      return node
+    }
+
+    return visit()
+  }
+}
