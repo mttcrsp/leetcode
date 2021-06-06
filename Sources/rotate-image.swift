69e5535641ea7e570d7c5425db1dceda181394f3
Matteo Crespi <crespi.matteo@icloud.com>
2021-06-06 17:03:52 +0200

    48. Rotate Image

---
+++ Sources/rotate-image.swift
@@ -0,0 +1,17 @@
+struct RotateImage {
+  func rotate(_ matrix: inout [[Int]]) {
+    let n = matrix.count
+
+    for i in 0 ..< n {
+      for j in i ..< n where i != j {
+        let tmp = matrix[j][i]
+        matrix[j][i] = matrix[i][j]
+        matrix[i][j] = tmp
+      }
+    }
+
+    for i in 0 ..< n {
+      matrix[i].reverse()
+    }
+  }
+}
