c95b014390fb7a4e1eec10fd4daa82c0e09e7b53
Matteo Crespi <crespi.matteo@icloud.com>
2021-06-06 11:55:26 +0200

    136. Single Number (tests)

---
+++ Tests/single-number-tests.swift
@@ -0,0 +1,23 @@
+@testable
+import Leetcode
+import XCTest
+
+final class SingleNumberTests: XCTestCase {
+  func testSingleNumber1() {
+    let input = [2, 2, 1]
+    let output = 1
+    XCTAssertEqual(SingleNumber().singleNumber(input), output)
+  }
+
+  func testSingleNumber2() {
+    let input = [4, 1, 2, 1, 2]
+    let output = 4
+    XCTAssertEqual(SingleNumber().singleNumber(input), output)
+  }
+
+  func testSingleNumber3() {
+    let input = [1]
+    let output = 1
+    XCTAssertEqual(SingleNumber().singleNumber(input), output)
+  }
+}
