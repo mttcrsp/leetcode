e4109f9aa3f7338ff1a7d5df2d46ff14bb86c7d3
Matteo Crespi <crespi.matteo@icloud.com>
2021-06-06 02:07:19 +0200

    122. Best Time to Buy and Sell Stock II (cont)

--- Sources/best-time-to-buy-and-sell-stock-ii.swift
+++ Sources/best-time-to-buy-and-sell-stock-ii.swift
@@ -1,21 +1,9 @@
 struct BestTimeToBuyAndSellStockIi {
   func maxProfit(_ prices: [Int]) -> Int {
-    guard let first = prices.first else { return 0 }
-
-    var profit = 0, min = first, max = first
+    var profit = 0
     for i in prices.indices.dropFirst() {
-      let prev = prices[i - 1]
-      let curr = prices[i]
-      if curr > prev {
-        max = curr
-      } else if curr < prev {
-        if max > min { profit += max - min }
-        min = curr
-        max = curr
-      }
+      profit += max(prices[i] - prices[i - 1], 0)
     }
-
-    if max > min { profit += max - min }
     return profit
   }
 }
