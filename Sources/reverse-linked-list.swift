1321c38453901f31136407d6ab7bf62dd84ff603
Matteo Crespi <crespi.matteo@icloud.com>
2021-06-14 00:14:25 +0200

    206. Reverse Linked List

---
+++ Sources/reverse-linked-list.swift
@@ -0,0 +1,14 @@
+struct ReverseLinkedList {
+  func reverseList(_ head: ListNode?) -> ListNode? {
+    var prev: ListNode?
+    var curr: ListNode? = head
+    var next: ListNode? = head?.next
+    while curr != nil {
+      next = curr?.next
+      curr?.next = prev
+      prev = curr
+      curr = next
+    }
+    return prev
+  }
+}
---
+++ Tests/reverse-linked-list-tests.swift
@@ -0,0 +1,28 @@
+@testable
+import Leetcode
+import XCTest
+
+final class ReverseLinkedListTests: XCTestCase {
+  func testReverseList1() {
+    let input = ListNode.makeList([1, 2, 3, 4, 5])
+    let output = ReverseLinkedList().reverseList(input)
+    XCTAssertEqual(output?.val, 5)
+    XCTAssertEqual(output?.next?.val, 4)
+    XCTAssertEqual(output?.next?.next?.val, 3)
+    XCTAssertEqual(output?.next?.next?.next?.val, 2)
+    XCTAssertEqual(output?.next?.next?.next?.next?.val, 1)
+  }
+
+  func testReverseList2() {
+    let input = ListNode.makeList([1, 2])
+    let output = ReverseLinkedList().reverseList(input)
+    XCTAssertEqual(output?.val, 2)
+    XCTAssertEqual(output?.next?.val, 1)
+  }
+
+  func testReverseList3() {
+    let input = ListNode.makeList([])
+    let output = ReverseLinkedList().reverseList(input)
+    XCTAssertNil(output)
+  }
+}
