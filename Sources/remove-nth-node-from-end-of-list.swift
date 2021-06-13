09e3d5019e5ea8123bf4ed64f34a110a26f19074
Matteo Crespi <crespi.matteo@icloud.com>
2021-06-13 23:58:22 +0200

    19. Remove Nth Node From End of List

---
+++ Sources/remove-nth-node-from-end-of-list.swift
@@ -0,0 +1,21 @@
+struct RemoveNthNodeFromEndOfList {
+  func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+    if head == nil { return nil }
+
+    var nodes: [ListNode] = []
+    var current = head
+
+    while let node = current {
+      nodes.append(node)
+      current = node.next
+    }
+
+    let index = nodes.count - n
+    if index == 0 {
+      return nodes[index].next
+    } else {
+      nodes[index - 1].next = nodes[index].next
+      return head
+    }
+  }
+}
---
+++ Tests/remove-nth-node-from-end-of-list-tests.swift
@@ -0,0 +1,26 @@
+@testable
+import Leetcode
+import XCTest
+
+final class RemoveNthNodeFromEndOfListTests: XCTestCase {
+  func testRemoveNthFromEnd1() {
+    let input = (ListNode.makeList([1, 2, 3, 4, 5]), 2)
+    let output = RemoveNthNodeFromEndOfList().removeNthFromEnd(input.0, input.1)
+    XCTAssertEqual(output?.val, 1)
+    XCTAssertEqual(output?.next?.val, 2)
+    XCTAssertEqual(output?.next?.next?.val, 3)
+    XCTAssertEqual(output?.next?.next?.next?.val, 5)
+  }
+
+  func testRemoveNthFromEnd2() {
+    let input = (ListNode.makeList([1]), 1)
+    let output = RemoveNthNodeFromEndOfList().removeNthFromEnd(input.0, input.1)
+    XCTAssertNil(output?.val)
+  }
+
+  func testRemoveNthFromEnd3() {
+    let input = (ListNode.makeList([1, 2]), 1)
+    let output = RemoveNthNodeFromEndOfList().removeNthFromEnd(input.0, input.1)
+    XCTAssertEqual(output?.val, 1)
+  }
+}
