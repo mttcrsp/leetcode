627da90f9d85618db6c0c561d57cf17159ad8974
Matteo Crespi <crespi.matteo@icloud.com>
2021-06-14 21:48:29 +0200

    101. Symmetric Tree

---
+++ Sources/symmetric-tree.swift
@@ -0,0 +1,18 @@
+struct SymmetricTree {
+  func isSymmetric(_ root: TreeNode?) -> Bool {
+    guard let root = root else { return true }
+
+    func visit(_ lhs: TreeNode?, _ rhs: TreeNode?) -> Bool {
+      switch (lhs, rhs) {
+      case (.none, .none):
+        return true
+      case (.some, .none), (.none, .some):
+        return false
+      case let (.some(lhs), .some(rhs)):
+        return lhs.val == rhs.val && visit(lhs.left, rhs.right) && visit(lhs.right, rhs.left)
+      }
+    }
+
+    return visit(root.left, root.right)
+  }
+}
---
+++ Tests/symmetric-tree-tests.swift
@@ -0,0 +1,17 @@
+@testable
+import Leetcode
+import XCTest
+
+final class SymmetricTreeTests: XCTestCase {
+  func testIsSymmetric1() {
+    let input = TreeNode.makeTree(with: [1, 2, 2, 3, 4, 4, 3])
+    let output = true
+    XCTAssertEqual(SymmetricTree().isSymmetric(input), output)
+  }
+
+  func testIsSymmetric2() {
+    let input = TreeNode.makeTree(with: [1, 2, 2, nil, 3, nil, 3])
+    let output = false
+    XCTAssertEqual(SymmetricTree().isSymmetric(input), output)
+  }
+}
