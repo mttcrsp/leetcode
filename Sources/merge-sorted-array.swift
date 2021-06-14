26d5aea677da3dda2dd6850f9d115136d01d4bcd
Matteo Crespi <crespi.matteo@icloud.com>
2021-06-15 01:19:07 +0200

    88. Merge Sorted Array

--- Sources/merge-sorted-array.swift
+++ Sources/merge-sorted-array.swift
@@ -1,23 +1,22 @@
 struct MergeSortedArray {
   func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-    var result: [Int] = []
+    var merged: [Int] = []
+
     var i = 0
     var j = 0
-
-    for _ in 1 ... n + m {
-      if i == m {
-        result.append(nums2[j]); j += 1
-      } else if j == n {
-        result.append(nums1[i]); i += 1
-      } else if nums1[i] < nums2[j] {
-        result.append(nums1[i]); i += 1
-      } else if nums1[i] >= nums2[j] {
-        result.append(nums2[j]); j += 1
+    while i < m, j < n {
+      if nums1[i] < nums2[j] {
+        merged.append(nums1[i]); i += 1
+      } else {
+        merged.append(nums2[j]); j += 1
       }
     }
 
-    for (x, number) in result.enumerated() {
-      nums1[x] = number
+    if i < m { merged.append(contentsOf: nums1[i ..< m]) }
+    if j < n { merged.append(contentsOf: nums2[j ..< n]) }
+
+    for (offset, number) in merged.enumerated() {
+      nums1[offset] = number
     }
   }
 }
