6cf0931f36b83d0e7649eb3cf6b6209b8d5a6af0
Matteo Crespi <crespi.matteo@icloud.com>
2021-07-08 00:04:46 +0200

    1736. Latest Time by Replacing Hidden Digits

---
+++ Sources/1736-latest-time-by-replacing-hidden-digits.swift
@@ -0,0 +1,31 @@
+struct LatestTimeByReplacingHiddenDigits {
+  func maximumTime(_ time: String) -> String {
+    var time = Array(time)
+
+    switch (time[0], time[1]) {
+    case ("?", "?"):
+      time[0] = "2"
+      time[1] = "3"
+    case ("?", "4"), ("?", "5"), ("?", "6"), ("?", "7"), ("?", "8"), ("?", "9"):
+      time[0] = "1"
+    case ("?", _):
+      time[0] = "2"
+    case ("2", "?"):
+      time[1] = "3"
+    case (_, "?"):
+      time[1] = "9"
+    case (_, _):
+      break
+    }
+
+    if time[3] == "?" {
+      time[3] = "5"
+    }
+
+    if time[4] == "?" {
+      time[4] = "9"
+    }
+
+    return String(time)
+  }
+}
