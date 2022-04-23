57432ff7030c62cd3e49a4301b0712fae7633ffd
Matteo Crespi <crespi.matteo@icloud.com>
2022-04-23 18:21:08 +0200

    94. Binary Tree Inorder Traversal

---
+++ Sources/0094-binary-tree-inorder-traversal.swift
@@ -0,0 +1,17 @@
+/// https://leetcode.com/problems/binary-tree-inorder-traversal/
+struct BinaryTreeInorderTraversal {
+  func inorderTraversal(_ root: TreeNode?) -> [Int] {
+    var result: [Int] = []
+
+    func visit(_ node: TreeNode?) {
+      guard let node = node else { return }
+      visit(node.left)
+      result.append(node.val)
+      visit(node.right)
+    }
+
+    visit(root)
+
+    return result
+  }
+}
---
+++ Tests/0094-binary-tree-inorder-traversal-tests.swift
@@ -0,0 +1,23 @@
+@testable
+import Leetcode
+import XCTest
+
+final class BinaryTreeInorderTraversalTests: XCTestCase {
+  func testTestInordertraversal1() {
+    let input = TreeNode.makeTree(with: [1, 3, 2])
+    let output = [3, 1, 2]
+    XCTAssertEqual(BinaryTreeInorderTraversal().inorderTraversal(input), output)
+  }
+
+  func testTestInordertraversal2() {
+    let input = TreeNode.makeTree(with: [])
+    let output = [] as [Int]
+    XCTAssertEqual(BinaryTreeInorderTraversal().inorderTraversal(input), output)
+  }
+
+  func testTestInordertraversal3() {
+    let input = TreeNode.makeTree(with: [1])
+    let output = [1]
+    XCTAssertEqual(BinaryTreeInorderTraversal().inorderTraversal(input), output)
+  }
+}
