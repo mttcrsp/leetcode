d0820411828a42ef3b0e408780d1fa47f6e48a98
Matteo Crespi <crespi.matteo@icloud.com>
2022-04-23 20:18:30 +0200

    326. Power of Three

---
+++ Tests/0326-power-of-three-tests.swift
@@ -0,0 +1,29 @@
+@testable
+import Leetcode
+import XCTest
+
+final class PowerOfThreeTests: XCTestCase {
+  func testTestIspowerofthree1() {
+    let input = 27
+    let output = true
+    XCTAssertEqual(PowerOfThree().isPowerOfThree(input), output)
+  }
+
+  func testTestIspowerofthree2() {
+    let input = 0
+    let output = false
+    XCTAssertEqual(PowerOfThree().isPowerOfThree(input), output)
+  }
+
+  func testTestIspowerofthree3() {
+    let input = 9
+    let output = true
+    XCTAssertEqual(PowerOfThree().isPowerOfThree(input), output)
+  }
+  
+  func testTestIspowerofthree4() {
+    let input = 1
+    let output = true
+    XCTAssertEqual(PowerOfThree().isPowerOfThree(input), output)
+  }
+}
