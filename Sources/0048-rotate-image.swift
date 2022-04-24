f3fb27c9fd80afb2297d0278c5dc81518c0c872b
Matteo Crespi <crespi.matteo@icloud.com>
2022-04-24 11:22:48 +0200

    48. Rotate Image

--- Sources/0048-rotate-image.swift
+++ Sources/0048-rotate-image.swift
@@ -1,18 +1,20 @@
 /// https://leetcode.com/problems/rotate-image/
 struct RotateImage {
   func rotate(_ matrix: inout [[Int]]) {
-    let n = matrix.count
+    var left = 0, right = matrix.count - 1
 
-    for i in 0 ..< n {
-      for j in i ..< n where i != j {
-        let tmp = matrix[j][i]
-        matrix[j][i] = matrix[i][j]
-        matrix[i][j] = tmp
+    while left < right {
+      for i in 0 ..< right - left {
+        let top = left, bottom = right
+        let topLeft = matrix[top][left + i]
+        matrix[top][left + i] = matrix[bottom - i][left]
+        matrix[bottom - i][left] = matrix[bottom][right - i]
+        matrix[bottom][right - i] = matrix[top + i][right]
+        matrix[top + i][right] = topLeft
       }
-    }
 
-    for i in 0 ..< n {
-      matrix[i].reverse()
+      left += 1
+      right -= 1
     }
   }
 }
