69e80c3180bcf8a09c08a995629f6b0af97fe170
Matteo Crespi <crespi.matteo@icloud.com>
2022-06-04 17:29:39 +0200

    11. Container With Most Water (cont)

--- Sources/0011-container-with-most-water.swift
+++ Sources/0011-container-with-most-water.swift
@@ -1,20 +1,19 @@
 /// https://leetcode.com/problems/container-with-most-water/
 struct ContainerWithMostWater {
-  func maxArea(_ heights: [Int]) -> Int {
+  func maxArea(_ height: [Int]) -> Int {
     var lhs = 0
-    var rhs = heights.count - 1
-    var max = min(heights[lhs], heights[rhs]) * (rhs - lhs)
+    var rhs = height.count - 1
+    var area = 0
 
     while lhs < rhs {
-      if heights[lhs] < heights[rhs] {
+      area = max(area, (rhs - lhs) * min(height[lhs], height[rhs]))
+      if height[lhs] < height[rhs] {
         lhs += 1
       } else {
         rhs -= 1
       }
-
-      max = Swift.max(max, min(heights[lhs], heights[rhs]) * (rhs - lhs))
     }
 
-    return max
+    return area
   }
 }
