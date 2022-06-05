681b9f64acf9f91d18e96754ce1eb9775d2d4b43
Matteo Crespi <crespi.matteo@icloud.com>
2022-06-06 01:03:39 +0200

    133. Clone Graph

--- Sources/0133-clone-graph.swift
+++ Sources/0133-clone-graph.swift
@@ -1,39 +1,33 @@
 /// https://leetcode.com/problems/clone-graph/
 struct CloneGraph {
   func cloneGraph(_ node: Node?) -> Node? {
-    guard let root = node else {
-      return nil
-    }
+    guard let node = node else { return nil }
 
-    var nodes: [Int: Node] = [:]
-    var clones: [Int: Node] = [:]
+    var dictionary: [Int: Node] = [:]
+    var stack: [Node] = [node]
+    while !stack.isEmpty {
+      let node = stack.removeLast()
 
-    var toVisit = [root]
-    while !toVisit.isEmpty {
-      let node = toVisit.removeLast()
-      nodes[node.val] = node
-      clones[node.val] = Node(node.val)
+      let copy = dictionary[node.val] ?? Node(node.val)
+      dictionary[node.val] = copy
 
-      for node in node.neighbors {
-        guard let node = node else { continue }
-        if nodes[node.val] == nil {
-          toVisit.append(node)
+      for neighbour in node.neighbors {
+        guard let neighbour = neighbour else { continue }
+
+        if dictionary[neighbour.val] == nil {
+          stack.append(neighbour)
         }
+
+        let neighbourCopy = dictionary[neighbour.val] ?? Node(neighbour.val)
+        dictionary[neighbour.val] = neighbourCopy
+        copy.neighbors.append(neighbourCopy)
       }
     }
 
-    for (_, clone) in clones {
-      for neighbor in nodes[clone.val]!.neighbors {
-        if let clonedNeighbor = clones[neighbor!.val] {
-          clone.neighbors.append(clonedNeighbor)
-        }
-      }
-    }
-
-    return clones[root.val]
+    return dictionary[node.val]
   }
 
-  class Node {
+  final class Node {
     var val: Int
     var neighbors: [Node?]
     init(_ val: Int) {
