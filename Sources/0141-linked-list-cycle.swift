51097543ed140d00b1a7462e29c404b47ecdd94f
Matteo Crespi <crespi.matteo@icloud.com>
2022-06-05 11:49:53 +0200

    141. Linked List Cycle

--- Sources/0141-linked-list-cycle.swift
+++ Sources/0141-linked-list-cycle.swift
@@ -1,14 +1,14 @@
 /// https://leetcode.com/problems/linked-list-cycle/
 struct LinkedListCycle {
   func hasCycle(_ head: ListNode?) -> Bool {
-    var visited: Set<Int> = []
-    var curr = head
-
-    while let node = curr {
-      let ptr = unsafeBitCast(node, to: Int.self)
-      let (inserted, _) = visited.insert(ptr)
-      if !inserted { return true }
-      curr = node.next
+    var slow = head
+    var fast = head
+    while fast != nil {
+      slow = slow?.next
+      fast = fast?.next?.next
+      if fast != nil, slow === fast {
+        return true
+      }
     }
 
     return false
