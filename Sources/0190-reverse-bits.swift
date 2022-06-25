b39b1c5d50e2d63b6dd082c7ade8b4616a7bd632
Matteo Crespi <crespi.matteo@icloud.com>
2022-06-25 15:36:32 +0200

    190. Reverse Bits (cont)

--- Sources/0190-reverse-bits.swift
+++ Sources/0190-reverse-bits.swift
@@ -2,10 +2,9 @@
 struct ReverseBits {
   func reverseBits(_ n: Int) -> Int {
     var reversed = 0
-    for i in 0 ..< 32 {
-      if n & (1 << i) > 0 {
-        reversed |= 1 << (31 - i)
-      }
+    for i in 0 ... 31 {
+      reversed = reversed << 1
+      reversed |= n & (1 << i) > 0 ? 1 : 0
     }
     return reversed
   }
