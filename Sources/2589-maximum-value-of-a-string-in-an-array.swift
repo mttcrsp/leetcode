ddcb89244a296771e1950001586683b922580a64
Matteo Crespi <crespi.matteo@icloud.com>
2023-03-04 17:45:35 +0100

    2496. Maximum Value of a String in an Array

---
+++ Sources/2589-maximum-value-of-a-string-in-an-array.swift
@@ -0,0 +1,26 @@
+/// https://leetcode.com/problems/maximum-value-of-a-string-in-an-array/
+struct MaximumValueOfAStringInAnArray {
+  func maximumValue(_ strings: [String]) -> Int {
+    var max = 0
+    
+    for string in strings {
+      var count = 0
+      var value = 0
+      var isNumber = true
+      
+      for character in string {
+        count += 1
+        if isNumber, let number = Int(String(character)) {
+          value *= 10
+          value += number
+        } else {
+          isNumber = false
+        }
+      }
+      
+      max = Swift.max(max, isNumber ? value : count)
+    }
+    
+    return max
+  }
+}
---
+++ Tests/2589-maximum-value-of-a-string-in-an-array-tests.swift
@@ -0,0 +1,29 @@
+@testable
+import Leetcode
+import XCTest
+
+final class MaximumValueOfAStringInAnArrayTests: XCTestCase {
+  func testTestMaximumvalue1() {
+    let input = ["alic3","bob","3","4","00000"]
+    let output = 5
+    XCTAssertEqual(MaximumValueOfAStringInAnArray().maximumValue(input), output)
+  }
+
+  func testTestMaximumvalue2() {
+    let input = ["1","01","001","0001"]
+    let output = 1
+    XCTAssertEqual(MaximumValueOfAStringInAnArray().maximumValue(input), output)
+  }
+  
+  func testTestMaximumvalue3() {
+    let input = ["00002","0a01"]
+    let output = 4
+    XCTAssertEqual(MaximumValueOfAStringInAnArray().maximumValue(input), output)
+  }
+  
+  func testTestMaximumvalue4() {
+    let input = ["12034"]
+    let output = 12034
+    XCTAssertEqual(MaximumValueOfAStringInAnArray().maximumValue(input), output)
+  }
+}
