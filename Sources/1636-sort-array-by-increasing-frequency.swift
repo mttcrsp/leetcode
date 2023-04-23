751906a72121a337441bd4f37d9b7d01ac1fd698
Matteo Crespi <crespi.matteo@icloud.com>
2023-04-23 17:54:23 +0200

    1636. Sort Array by Increasing Frequency

---
+++ Sources/1636-sort-array-by-increasing-frequency.swift
@@ -0,0 +1,27 @@
+/// https://leetcode.com/problems/sort-array-by-increasing-frequency/
+struct SortArrayByIncreasingFrequency {
+  func frequencySort(_ nums: [Int]) -> [Int] {
+    var frequencies: [Int: Int] = [:]
+    for num in nums {
+      frequencies[num, default: 0] += 1
+    }
+    
+    let sortedFrequencies = frequencies.sorted { lhs, rhs in
+      if lhs.value == rhs.value {
+        return lhs.key > rhs.key
+      } else {
+        return lhs.value < rhs.value
+      }
+    }
+    
+    var index = 0
+    var result = nums
+    for (num, count) in sortedFrequencies {
+      for _ in 0 ..< count {
+        result[index] = num
+        index += 1
+      }
+    }
+    return result
+  }
+}
