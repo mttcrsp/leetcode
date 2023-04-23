68c34f95a904fb9a7513bc2e603e5ea8a20977fb
Matteo Crespi <crespi.matteo@icloud.com>
2023-04-23 17:55:11 +0200

    1624. Largest Substring Between Two Equal Characters

---
+++ Sources/1624-largest-substring-between-two-equal-characters.swift
@@ -0,0 +1,15 @@
+/// https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+struct LargestSubstringBetweenTwoEqualCharacters {
+  func maxLengthBetweenEqualCharacters(_ s: String) -> Int {
+    var max = -1
+    var firstOccurrences: [Character: Int] = [:]
+    for (index, character) in Array(s).enumerated() {
+      if let firstOccurrence = firstOccurrences[character] {
+        max = Swift.max(max, index - firstOccurrence - 1)
+      } else {
+        firstOccurrences[character] = index
+      }
+    }
+    return max
+  }
+}
---
+++ Tests/1624-largest-substring-between-two-equal-characters-tests.swift
@@ -0,0 +1,29 @@
+@testable
+import Leetcode
+import XCTest
+
+final class LargestSubstringBetweenTwoEqualCharactersTests: XCTestCase {
+  func testTestMaxlengthbetweenequalcharacters1() {
+    let input = "aa"
+    let output = 0
+    XCTAssertEqual(LargestSubstringBetweenTwoEqualCharacters().maxLengthBetweenEqualCharacters(input), output)
+  }
+
+  func testTestMaxlengthbetweenequalcharacters2() {
+    let input = "abca"
+    let output = 2
+    XCTAssertEqual(LargestSubstringBetweenTwoEqualCharacters().maxLengthBetweenEqualCharacters(input), output)
+  }
+
+  func testTestMaxlengthbetweenequalcharacters3() {
+    let input = "cbzxy"
+    let output = -1
+    XCTAssertEqual(LargestSubstringBetweenTwoEqualCharacters().maxLengthBetweenEqualCharacters(input), output)
+  }
+  
+  func testTestMaxlengthbetweenequalcharacters4() {
+    let input = "cccccccccccc"
+    let output = 10
+    XCTAssertEqual(LargestSubstringBetweenTwoEqualCharacters().maxLengthBetweenEqualCharacters(input), output)
+  }
+}
