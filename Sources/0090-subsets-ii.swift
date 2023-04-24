b36465f44c68683fdce604c2fcfcd186fde9c3a3
Matteo Crespi <crespi.matteo@icloud.com>
2023-04-24 18:50:11 +0200

    90. Subsets II

---
+++ Sources/0090-subsets-ii.swift
@@ -0,0 +1,27 @@
+/// https://leetcode.com/problems/subsets-ii/
+struct SubsetsIi {
+  func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
+    var matchedOccurrences: Set<[Int: Int]> = []
+    var stack: [(offset: Int, subset: [Int], occurrences: [Int: Int])] = [(0, [], [:])]
+    var result: [[Int]] = [[]]
+    
+    while let (offset, subset, occurrences) = stack.popLast() {
+      guard offset < nums.count else { continue }
+      
+      let num = nums[offset]
+      var newOccurrences = occurrences
+      newOccurrences[num, default: 0] += 1
+      
+      let newSubset = subset + [num]
+      stack.append((offset + 1, subset, occurrences))
+      
+      if !matchedOccurrences.contains(newOccurrences) {
+        matchedOccurrences.insert(newOccurrences)
+        result.append(newSubset)
+        stack.append((offset + 1, newSubset, newOccurrences))
+      }
+    }
+    
+    return result
+  }
+}
---
+++ Tests/0090-subsets-ii-tests.swift
@@ -0,0 +1,17 @@
+@testable
+import Leetcode
+import XCTest
+
+final class SubsetsIiTests: XCTestCase {
+  func testTestSubsetswithdup1() {
+    let input = [1,2,2]
+    let output = [[],[1],[1,2],[1,2,2],[2],[2,2]]
+    XCTAssertEqual(Set(SubsetsIi().subsetsWithDup(input)), Set(output))
+  }
+
+  func testTestSubsetswithdup2() {
+    let input = [0]
+    let output = [[],[0]]
+    XCTAssertEqual(Set(SubsetsIi().subsetsWithDup(input)), Set(output))
+  }
+}
