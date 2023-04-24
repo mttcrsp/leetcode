50a46354da7284d8ef8f973eef4b221d6ffc6fb2
Matteo Crespi <crespi.matteo@icloud.com>
2023-04-24 17:58:08 +0200

    46. Permutations (cont)

--- Sources/0046-permutations.swift
+++ Sources/0046-permutations.swift
@@ -2,19 +2,19 @@
 struct Permutations {
   func permute(_ nums: [Int]) -> [[Int]] {
     var result: [[Int]] = []
+    var stack: [([Int], Set<Int>)] = [([], Set(nums))]
     
-    func visit(permutation: [Int] = [], unused: Set<Int> = Set(nums)) {
+    while let (permutation, unused) = stack.popLast() {
       if unused.isEmpty {
         result.append(permutation)
       } else {
         for element in unused {
           let newUnused = unused.subtracting([element])
           let newPermutation = permutation + [element]
-          visit(permutation: newPermutation, unused: newUnused)
+          stack.append((newPermutation, newUnused))
         }
       }
     }
-    visit()
     
     return result
   }
