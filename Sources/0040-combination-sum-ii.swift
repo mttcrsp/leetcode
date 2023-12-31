4ddf2f9e6607022a1468735011efb371763e99af
Matteo Crespi <crespi.matteo@icloud.com>
2023-12-31 15:24:39 +0100

    40. Combination Sum II (cont)

--- Sources/0040-combination-sum-ii.swift
+++ Sources/0040-combination-sum-ii.swift
@@ -1,21 +1,29 @@
 /// https://leetcode.com/problems/combination-sum-ii/
 struct CombinationSumIi {
   func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
-    var combinations: [[Int]: Int] = [[]: 0]
-    for candidate in candidates {
-      for (combination, sum) in combinations {
-        if sum+candidate <= target {
-          combinations[combination+[candidate]] = sum+candidate
+    let candidates = candidates.sorted()
+
+    var result: [[Int]] = []
+    var current: [Int] = []
+    var currentDifference = target
+    func backtrack(_ index: Int) {
+      if currentDifference == 0 {
+        result.append(current)
+      } else if currentDifference > 0 {
+        var prev = -1
+        for i in index ..< candidates.count {
+          guard candidates[i] != prev else { continue }
+          current.append(candidates[i])
+          currentDifference -= candidates[i]
+          backtrack(i+1)
+          currentDifference += candidates[i]
+          current.removeLast()
+          prev = candidates[i]
         }
       }
     }
 
-    var result: Set<[Int]> = []
-    for (combination, sum) in combinations {
-      if sum == target {
-        result.insert(combination.sorted())
-      }
-    }
-    return Array(result)
+    backtrack(0)
+    return result
   }
 }
