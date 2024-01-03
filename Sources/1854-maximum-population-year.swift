2e91d86c830970303f11da2c513f742d98333c34
Matteo Crespi <crespi.matteo@icloud.com>
2024-01-03 23:02:46 +0100

    1854. Maximum Population Year (cont)

--- Sources/1854-maximum-population-year.swift
+++ Sources/1854-maximum-population-year.swift
@@ -1,18 +1,25 @@
-/// https://leetcode.com/problems/maximum-population-year/
+/// https://leetcode.com/problems/maximum-population-counts/
 struct MaximumPopulationYear {
   func maximumPopulation(_ logs: [[Int]]) -> Int {
     let minYear = 1950
     let maxYear = 2050
 
-    var population = [Int](repeating: 0, count: maxYear-minYear)
+    var counts = [Int](repeating: 0, count: maxYear-minYear+1)
     for log in logs {
-      for i in log[0]-minYear ..< log[1]-minYear {
-        population[i] += 1
+      counts[log[0]-minYear] += 1
+      counts[log[1]-minYear] -= 1
+    }
+
+    var maxCount = counts[0]
+    var maxIndex = 0
+    for index in counts.indices.dropFirst() {
+      counts[index] += counts[index-1]
+      if counts[index] > maxCount {
+        maxCount = counts[index]
+        maxIndex = index
       }
     }
 
-    return population.enumerated()
-      .max { $0.element < $1.element }!
-      .offset+minYear
+    return minYear+maxIndex
   }
 }
