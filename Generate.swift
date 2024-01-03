d10bef84889246d91b5962c59cc95f9a15543f70
Matteo Crespi <crespi.matteo@icloud.com>
2024-01-03 02:00:13 +0100

    Simplify question slug copy-pasting

--- Generate.swift
+++ Generate.swift
@@ -42,7 +42,11 @@ func main() async {
     preconditionFailure("Expected a Leetcode CSRF token to be provided as the environment variable LEETCODE_CSRF_TOKEN.")
   }
 
-  let questionTitleSlug = arguments[1]
+  var questionTitleSlug = arguments[1]
+  if questionTitleSlug.hasSuffix("/") {
+    questionTitleSlug.removeLast()
+  }
+
   let parameters = "{\"operationName\":\"questionData\",\"variables\":{\"titleSlug\":\"\(questionTitleSlug)\"},\"query\":\"query questionData($titleSlug: String!) {\\n  question(titleSlug: $titleSlug) {\\n    questionId\\n    questionFrontendId\\n    boundTopicId\\n    title\\n    titleSlug\\n    content\\n    translatedTitle\\n    translatedContent\\n    isPaidOnly\\n    difficulty\\n    likes\\n    dislikes\\n    isLiked\\n    similarQuestions\\n    exampleTestcases\\n    categoryTitle\\n    contributors {\\n      username\\n      profileUrl\\n      avatarUrl\\n      __typename\\n    }\\n    topicTags {\\n      name\\n      slug\\n      translatedName\\n      __typename\\n    }\\n    companyTagStats\\n    codeSnippets {\\n      lang\\n      langSlug\\n      code\\n      __typename\\n    }\\n __typename\\n  }\\n}\\n\"}"
   let postData = parameters.data(using: .utf8)
   var request = URLRequest(url: URL(string: "https://leetcode.com/graphql")!, timeoutInterval: Double.infinity)
