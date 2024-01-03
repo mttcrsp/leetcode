9e6529f08e33a3ebc8ebf38b1854efef8654334e
Matteo Crespi <crespi.matteo@icloud.com>
2024-01-03 18:57:05 +0100

    Support for URL based generation

--- Generate.swift
+++ Generate.swift
@@ -43,8 +43,12 @@ func main() async {
   }
 
   var questionTitleSlug = arguments[1]
-  if questionTitleSlug.hasSuffix("/") {
-    questionTitleSlug.removeLast()
+  if let url = URL(string: questionTitleSlug) {
+    if url.pathComponents.count > 2, url.pathComponents.prefix(2) == ["/", "problems"] {
+      questionTitleSlug = url.pathComponents[2]
+    } else {
+      preconditionFailure("Unexpected Leetcode URL format '\(url)'")
+    }
   }
 
   let parameters = "{\"operationName\":\"questionData\",\"variables\":{\"titleSlug\":\"\(questionTitleSlug)\"},\"query\":\"query questionData($titleSlug: String!) {\\n  question(titleSlug: $titleSlug) {\\n    questionId\\n    questionFrontendId\\n    boundTopicId\\n    title\\n    titleSlug\\n    content\\n    translatedTitle\\n    translatedContent\\n    isPaidOnly\\n    difficulty\\n    likes\\n    dislikes\\n    isLiked\\n    similarQuestions\\n    exampleTestcases\\n    categoryTitle\\n    contributors {\\n      username\\n      profileUrl\\n      avatarUrl\\n      __typename\\n    }\\n    topicTags {\\n      name\\n      slug\\n      translatedName\\n      __typename\\n    }\\n    companyTagStats\\n    codeSnippets {\\n      lang\\n      langSlug\\n      code\\n      __typename\\n    }\\n __typename\\n  }\\n}\\n\"}"
