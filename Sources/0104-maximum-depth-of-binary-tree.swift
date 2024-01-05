53526e7d0d657adcaaf9a9231cbc569191709774
Matteo Crespi <crespi.matteo@icloud.com>
2024-01-05 02:40:46 +0100

    104. Maximum Depth of Binary Tree (cont)

--- Sources/0104-maximum-depth-of-binary-tree.swift
+++ Sources/0104-maximum-depth-of-binary-tree.swift
@@ -1,18 +1,9 @@
 /// https://leetcode.com/problems/maximum-depth-of-binary-tree/
 struct MaximumDepthOfBinaryTree {
   func maxDepth(_ root: TreeNode?) -> Int {
-    var stack: [(TreeNode?, depth: Int)] = [(root, 1)]
-    var maxDepth = 0
-
-    while !stack.isEmpty {
-      let (node, depth) = stack.removeFirst()
-      if let node {
-        maxDepth = max(maxDepth, depth)
-        stack.append((node.left, depth+1))
-        stack.append((node.right, depth+1))
-      }
-    }
-
-    return maxDepth
+    guard let root else { return 0 }
+    let lhsDepth = maxDepth(root.left)
+    let rhsDepth = maxDepth(root.right)
+    return max(lhsDepth, rhsDepth)+1
   }
 }
