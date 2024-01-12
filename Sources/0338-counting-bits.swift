0eb8e5be7695e13985a6cb35d40f162fbca3dde2
Matteo Crespi <crespi.matteo@icloud.com>
2024-01-12 02:14:37 +0100

    338. Counting Bits (cont)

--- Sources/0338-counting-bits.swift
+++ Sources/0338-counting-bits.swift
@@ -1,18 +1,12 @@
 /// https://leetcode.com/problems/counting-bits/
 struct CountingBits {
   func countBits(_ n: Int) -> [Int] {
-    guard n > 0 else {
-      return [0]
-    }
+    guard n > 0 else { return [0] }
 
     var result = [Int](repeating: 0, count: n+1)
-    var j = 0
     for i in 1 ... n {
-      j = i.nonzeroBitCount == 1 ? 0 : j
-      result[i] = result[j]+1
-      j += 1
+      result[i] = result[i/2]+(i%2)
     }
-
     return result
   }
 }
