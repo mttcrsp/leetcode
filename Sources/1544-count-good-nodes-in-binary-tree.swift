edb553780a803e5539de83a25b14555caacd7d4b
Matteo Crespi <crespi.matteo@icloud.com>
2024-01-14 15:38:12 +0100

    1448. Count Good Nodes in Binary Tree (cont)

--- Sources/1544-count-good-nodes-in-binary-tree.swift
+++ Sources/1544-count-good-nodes-in-binary-tree.swift
@@ -1,23 +1,21 @@
 /// https://leetcode.com/problems/count-good-nodes-in-binary-tree/
 struct CountGoodNodesInBinaryTree {
   func goodNodes(_ root: TreeNode?) -> Int {
-    var stack: [(TreeNode?, Int)] = [(root, Int.min)]
+    guard let root else { return 0 }
+
     var count = 0
-
-    while !stack.isEmpty {
-      let (node, pathMax) = stack.removeLast()
-
-      guard let node else {
-        continue
-      }
-
+    var stack = [(root, Int.min)]
+    while let (node, pathMax) = stack.popLast() {
       if pathMax <= node.val {
         count += 1
       }
 
       let newPathMax = max(pathMax, node.val)
-      stack.append((node.left, newPathMax))
-      stack.append((node.right, newPathMax))
+      for child in [node.left, node.right] {
+        if let child {
+          stack.append((child, newPathMax))
+        }
+      }
     }
 
     return count
