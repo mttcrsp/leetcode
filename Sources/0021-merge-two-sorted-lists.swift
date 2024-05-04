f095a2bddea3f6bf501a89156ad8225ead6140f0
Matteo Crespi <crespi.matteo@icloud.com>
2024-05-04 22:38:54 +0200

    21. Merge Two Sorted Lists

--- Sources/0021-merge-two-sorted-lists.swift
+++ Sources/0021-merge-two-sorted-lists.swift
@@ -1,36 +1,29 @@
 /// https://leetcode.com/problems/merge-two-sorted-lists/
 struct MergeTwoSortedLists {
   func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-    var head: ListNode?
     var list1 = list1
     var list2 = list2
-    var mergedList: ListNode? {
-      didSet {
-        if head == nil {
-          head = mergedList
-        }
-      }
+    var head: ListNode?
+    var curr: ListNode? {
+      didSet { head = head ?? curr }
     }
 
-    let consume: (inout ListNode?) -> Void = { list in
-      mergedList?.next = list
-      mergedList = list
-      list = list?.next
+    func advance(_ node: inout ListNode?) {
+      curr?.next = node
+      curr = node
+      node = node?.next
     }
 
-    while true {
+    loop: while true {
       switch (list1, list2) {
-      case let (node1?, node2?) where node1.val < node2.val:
-        consume(&list1)
-      case (.some, .some):
-        consume(&list2)
-      case (.some, .none):
-        consume(&list1)
-      case (.none, .some):
-        consume(&list2)
-      case (nil, nil):
-        return head
+      case let (node1?, node2?) where node1.val < node2.val: advance(&list1)
+      case (.some, .some): advance(&list2)
+      case (.some, .none): advance(&list1)
+      case (.none, .some): advance(&list2)
+      case (nil, nil): break loop
       }
     }
+
+    return head
   }
 }
