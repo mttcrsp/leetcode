a248f9a457902eb87762de99edad9c740f9854aa
Matteo Crespi <crespi.matteo@icloud.com>
2024-05-07 21:31:37 +0200

    297. Serialize and Deserialize Binary Tree

--- Sources/0297-serialize-and-deserialize-binary-tree.swift
+++ Sources/0297-serialize-and-deserialize-binary-tree.swift
@@ -1,40 +1,48 @@
 /// https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
-class Codec {
+class SerializeAndDeserializeBinaryTreeCodec {
   func serialize(_ root: TreeNode?) -> String {
-    var values: [String] = []
-
-    func visit(_ node: TreeNode?) {
-      guard let node else {
-        return values.append("n")
+    var preorder: [Int] = []
+    var inorder: [Int] = []
+    var stack: [TreeNode] = []
+    var curr = root
+    while curr != nil || !stack.isEmpty {
+      if let node = curr {
+        preorder.append(node.val)
+        stack.append(node)
+        curr = node.left
+      } else {
+        let node = stack.removeLast()
+        inorder.append(node.val)
+        curr = node.right
       }
-
-      values.append(node.val.description)
-      visit(node.left)
-      visit(node.right)
     }
 
-    visit(root)
-
-    return values.joined(separator: ",")
+    let serializedPreorder = preorder.map(\.description).joined(separator: ",")
+    let serializedInorder = inorder.map(\.description).joined(separator: ",")
+    return [serializedPreorder, serializedInorder].joined(separator: separator)
   }
 
-  func deserialize(_ serialized: String) -> TreeNode? {
-    let values = serialized.components(separatedBy: ",").map(Int.init)
-    var index = 0
+  private let separator = "|"
 
-    func visit() -> TreeNode? {
-      guard let value = values[index] else {
-        index += 1
-        return nil
-      }
+  func deserialize(_ data: String) -> TreeNode? {
+    let components = data.components(separatedBy: separator)
+    guard
+      components.count == 2,
+      let serializedPreorder = components.first,
+      let serializedInorder = components.last
+    else { preconditionFailure("malformed representation") }
 
-      index += 1
-      let node = TreeNode(value)
-      node.left = visit()
-      node.right = visit()
+    let preorder = serializedPreorder.components(separatedBy: ",").compactMap(Int.init)
+    let inorder = serializedInorder.components(separatedBy: ",").compactMap(Int.init)
+
+    func visit(_ preorder: [Int], _ inorder: [Int]) -> TreeNode? {
+      guard let val = preorder.first, let mid = inorder.firstIndex(of: val) else { return nil }
+      let node = TreeNode(val)
+      node.left = visit(Array(preorder[1 ..< mid+1]), Array(inorder[..<mid]))
+      node.right = visit(Array(preorder[(mid+1)...]), Array(inorder[(mid+1)...]))
       return node
     }
 
-    return visit()
+    return visit(preorder, inorder)
   }
 }
--- Tests/0297-serialize-and-deserialize-binary-tree-tests.swift
+++ Tests/0297-serialize-and-deserialize-binary-tree-tests.swift
@@ -5,51 +5,43 @@ import XCTest
 final class SerializeAndDeserializeBinaryTreeTests: XCTestCase {
   func testCodec1() {
     let tree = TreeNode([1, 2, 3, nil, nil, 4, 5])
-    let serialized = Codec().serialize(tree)
-    let deserialized = Codec().deserialize(serialized)
-    XCTAssertEqual(tree?.testDescription, deserialized?.testDescription)
+    let serialized = SerializeAndDeserializeBinaryTreeCodec().serialize(tree)
+    let deserialized = SerializeAndDeserializeBinaryTreeCodec().deserialize(serialized)
+    XCTAssertEqual(tree, deserialized)
   }
 
   func testCodec2() {
     let tree = TreeNode([])
-    let serialized = Codec().serialize(tree)
-    let deserialized = Codec().deserialize(serialized)
-    XCTAssertEqual(tree?.testDescription, deserialized?.testDescription)
+    let serialized = SerializeAndDeserializeBinaryTreeCodec().serialize(tree)
+    let deserialized = SerializeAndDeserializeBinaryTreeCodec().deserialize(serialized)
+    XCTAssertEqual(tree, deserialized)
   }
 
   func testCodec3() {
     let tree = TreeNode([1])
-    let serialized = Codec().serialize(tree)
-    let deserialized = Codec().deserialize(serialized)
-    XCTAssertEqual(tree?.testDescription, deserialized?.testDescription)
+    let serialized = SerializeAndDeserializeBinaryTreeCodec().serialize(tree)
+    let deserialized = SerializeAndDeserializeBinaryTreeCodec().deserialize(serialized)
+    XCTAssertEqual(tree, deserialized)
   }
 
   func testCodec4() {
     let tree = TreeNode([1, 2])
-    let serialized = Codec().serialize(tree)
-    let deserialized = Codec().deserialize(serialized)
-    XCTAssertEqual(tree?.testDescription, deserialized?.testDescription)
+    let serialized = SerializeAndDeserializeBinaryTreeCodec().serialize(tree)
+    let deserialized = SerializeAndDeserializeBinaryTreeCodec().deserialize(serialized)
+    XCTAssertEqual(tree, deserialized)
   }
 
   func testCodec5() {
     let tree = TreeNode([4, -7, -3, nil, nil, -9, -3, 9, -7, -4, nil, 6, nil, -6, -6, nil, nil, 0, 6, 5, nil, 9, nil, nil, -1, -4, nil, nil, nil, -2])
-    let serialized = Codec().serialize(tree)
-    let deserialized = Codec().deserialize(serialized)
-    XCTAssertEqual(tree?.testDescription, deserialized?.testDescription)
+    let serialized = SerializeAndDeserializeBinaryTreeCodec().serialize(tree)
+    let deserialized = SerializeAndDeserializeBinaryTreeCodec().deserialize(serialized)
+    XCTAssertEqual(tree, deserialized)
   }
 
   func testCodec6() {
     let tree = TreeNode([-1, -2])
-    let serialized = Codec().serialize(tree)
-    let deserialized = Codec().deserialize(serialized)
-    XCTAssertEqual(tree?.testDescription, deserialized?.testDescription)
-  }
-}
-
-private extension TreeNode {
-  var testDescription: String {
-    var description = ""
-    dump(self, to: &description)
-    return description
+    let serialized = SerializeAndDeserializeBinaryTreeCodec().serialize(tree)
+    let deserialized = SerializeAndDeserializeBinaryTreeCodec().deserialize(serialized)
+    XCTAssertEqual(tree, deserialized)
   }
 }
