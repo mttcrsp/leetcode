e82560847d02e89ec28ebaac9fd48dff323a7b88
Matteo Crespi <crespi.matteo@icloud.com>
2024-05-10 19:03:24 +0200

    1249. Minimum Remove to Make Valid Parentheses

--- Sources/1249-minimum-remove-to-make-valid-parentheses.swift
+++ Sources/1249-minimum-remove-to-make-valid-parentheses.swift
@@ -1,31 +1,32 @@
 /// https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
 struct MinimumRemoveToMakeValidParentheses {
   func minRemoveToMakeValid(_ s: String) -> String {
-    var result = ""
-    var unmatched: [String.Index] = []
-    var index = s.startIndex
-    while index < s.endIndex {
-      defer { index = s.index(after: index) }
-
-      let character = s[index]
-      switch character {
-      case "(":
-        unmatched.append(result.endIndex)
-        result.append(character)
-      case ")":
-        if !unmatched.isEmpty {
-          unmatched.removeLast()
-          result.append(character)
-        }
-      case _:
-        result.append(character)
+    var open = 0, close = 0
+    var forward = ""
+    for character in s {
+      switch (character, open > close) {
+      case ("(", _): forward.append(character); open += 1
+      case (")", true): forward.append(character); close += 1
+      case (")", false): continue
+      case (_, _): forward.append(character)
       }
     }
 
-    for index in unmatched.reversed() {
-      result.remove(at: index)
+    guard open != close else {
+      return forward
     }
 
-    return result
+    open = 0; close = 0
+    var backward = ""
+    for character in forward.reversed() {
+      switch (character, close > open) {
+      case (")", _): backward.append(character); close += 1
+      case ("(", true): backward.append(character); open += 1
+      case ("(", false): continue
+      case (_, _): backward.append(character)
+      }
+    }
+
+    return String(backward.reversed())
   }
 }
