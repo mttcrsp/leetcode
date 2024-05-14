92a54035b7cdb6949fcd0094db273343d45c56f9
Matteo Crespi <crespi.matteo@icloud.com>
2024-05-14 20:46:02 +0200

    973. K Closest Points to Origin

--- Sources/0973-k-closest-points-to-origin.swift
+++ Sources/0973-k-closest-points-to-origin.swift
@@ -1,15 +1,35 @@
+import Collections
+
 /// https://leetcode.com/problems/k-closest-points-to-origin/
 struct KClosestPointsToOrigin {
   func kClosest(_ points: [[Int]], _ K: Int) -> [[Int]] {
-    let sortedFromOrigin = points.sorted { lhs, rhs in
-      lhs.distanceFromOrigin() < rhs.distanceFromOrigin()
+    var heap: Heap<Element> = []
+    for point in points {
+      heap.insert(Element(point: point))
+      if heap.count > K {
+        _ = heap.removeMax()
+      }
     }
-    return Array(sortedFromOrigin.prefix(K))
-  }
-}
 
-private extension [Int] {
-  func distanceFromOrigin() -> Int {
-    (self[0]*self[0])+(self[1]*self[1])
+    var result: [[Int]] = []
+    for _ in 0 ..< heap.count {
+      result.append(heap.removeMin().point)
+    }
+
+    return result
+  }
+
+  struct Element: Comparable {
+    let point: [Int]
+
+    var distance: Double {
+      let xSquared = point[0]*point[0]
+      let ySquared = point[1]*point[1]
+      return Double(xSquared+ySquared).squareRoot()
+    }
+
+    static func < (_ lhs: Element, _ rhs: Element) -> Bool {
+      lhs.distance < rhs.distance
+    }
   }
 }
