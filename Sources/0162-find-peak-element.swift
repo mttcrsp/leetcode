e1485b86fed5d9d86b3ecd8d6072b74971454638
Matteo Crespi <crespi.matteo@icloud.com>
2024-05-14 20:35:41 +0200

    162. Find Peak Element

--- Sources/0162-find-peak-element.swift
+++ Sources/0162-find-peak-element.swift
@@ -1,21 +1,22 @@
 /// https://leetcode.com/problems/find-peak-element/
 struct FindPeakElement {
   func findPeakElement(_ nums: [Int]) -> Int {
-    var lhs = -1
-    var rhs = nums.count
-    while lhs <= rhs {
+    var lhs = 0
+    var rhs = nums.count-1
+    while lhs < rhs {
       let mid = (lhs+rhs)/2
-      let midValue = nums[mid]
-      let prevValue: Int = mid-1 == -1 ? .min : nums[mid-1]
-      let nextValue: Int = mid+1 == nums.count ? .min : nums[mid+1]
-      if prevValue > midValue {
+      let curr = nums[mid]
+      let prev = nums.indices ~= mid-1 ? nums[mid-1] : .min
+      let next = nums.indices ~= mid+1 ? nums[mid+1] : .min
+      if curr < prev {
         rhs = mid-1
-      } else if nextValue > midValue {
+      } else if curr < next {
         lhs = mid+1
       } else {
         return mid
       }
     }
-    return -1
+
+    return lhs
   }
 }
