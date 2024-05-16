3d8fdaeedb5ee3ed5e3d6087a5d88e0760de2d7c
Matteo Crespi <crespi.matteo@icloud.com>
2024-05-17 00:19:45 +0200

    398. Random Pick Index

---
+++ Sources/0398-random-pick-index.swift
@@ -0,0 +1,21 @@
+
+/// https://leetcode.com/problems/random-pick-index/
+struct RandomPickIndex {
+  private let indices: [Int: [Int]]
+
+  init(_ nums: [Int]) {
+    var indices: [Int: [Int]] = [:]
+    for (index, num) in nums.enumerated() {
+      indices[num, default: []].append(index)
+    }
+    self.indices = indices
+  }
+
+  func pick(_ target: Int) -> Int {
+    if let index = indices[target]?.randomElement() {
+      index
+    } else {
+      -1
+    }
+  }
+}
