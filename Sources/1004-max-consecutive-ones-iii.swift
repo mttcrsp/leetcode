06b96cb91df2c7efef656e850373fa248c387119
Matteo Crespi <crespi.matteo@icloud.com>
2024-05-20 20:39:27 +0200

    1004. Max Consecutive Ones III

--- Sources/1004-max-consecutive-ones-iii.swift
+++ Sources/1004-max-consecutive-ones-iii.swift
@@ -1,22 +1,18 @@
 /// https://leetcode.com/problems/max-consecutive-ones-iii/
 struct MaxConsecutiveOnesIii {
   func longestOnes(_ nums: [Int], _ k: Int) -> Int {
-    var longest = 0
-    var zeros = 0
+    var result = 0
+    var zerosCount = 0
     var lhs = 0
-    var rhs = 0
-    while lhs < nums.count, rhs < nums.count {
-      if zeros <= k {
-        zeros += nums[rhs] == 0 ? 1 : 0
-        if zeros <= k {
-          longest = max(longest, rhs-lhs+1)
-        }
-        rhs += 1
-      } else {
-        zeros -= nums[lhs] == 0 ? 1 : 0
+    for rhs in nums.indices {
+      zerosCount += nums[rhs] == 0 ? 1 : 0
+      while zerosCount > k {
+        zerosCount -= nums[lhs] == 0 ? 1 : 0
         lhs += 1
       }
+      result = max(result, rhs-lhs+1)
     }
-    return longest
+
+    return result
   }
 }
