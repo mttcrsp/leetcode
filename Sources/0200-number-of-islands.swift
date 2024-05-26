d30f6c914a9faca257b844f5409c44858e559ec4
Matteo Crespi <crespi.matteo@icloud.com>
2024-05-26 18:36:09 +0200

    200. Number of Islands

--- Sources/0200-number-of-islands.swift
+++ Sources/0200-number-of-islands.swift
@@ -1,32 +1,36 @@
 /// https://leetcode.com/problems/number-of-islands/
 struct NumberOfIslands {
   func numIslands(_ grid: [[Character]]) -> Int {
-    struct Coordinate: Hashable {
-      let x, y: Int
-    }
+    let directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-    var result = 0
+    let rows = grid.count
+    let cols = grid[0].count
     var grid = grid
+    var islandsCount = 0
+    for row in grid.indices {
+      for col in grid[row].indices {
+        if grid[row][col] == "1" {
+          islandsCount += 1
 
-    func eraseIsland(at x: Int, _ y: Int) {
-      if x >= 0, x < grid.count, y >= 0, y < grid[x].count, grid[x][y] == "1" {
-        grid[x][y] = "0"
-        eraseIsland(at: x+1, y)
-        eraseIsland(at: x-1, y)
-        eraseIsland(at: x, y+1)
-        eraseIsland(at: x, y-1)
-      }
-    }
-
-    for x in grid.indices {
-      for y in grid[0].indices {
-        if grid[x][y] == "1" {
-          result += 1
-          eraseIsland(at: x, y)
+          var queue: [[Int]] = [[row, col]]
+          while let slot = queue.popLast() {
+            grid[slot[0]][slot[1]] = "0"
+            for direction in directions {
+              var nextSlot = slot
+              nextSlot[0] += direction[0]
+              nextSlot[1] += direction[1]
+              guard
+                nextSlot[0] >= 0, nextSlot[0] < rows,
+                nextSlot[1] >= 0, nextSlot[1] < cols,
+                grid[nextSlot[0]][nextSlot[1]] == "1"
+              else { continue }
+              queue.append(nextSlot)
+            }
+          }
         }
       }
     }
 
-    return result
+    return islandsCount
   }
 }
