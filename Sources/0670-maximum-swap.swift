f9ae0c0350868195bb4719c5c3f3f4edf9dd888f
Matteo Crespi <crespi.matteo@icloud.com>
2024-05-30 11:56:55 +0200

    670. Maximum Swap

--- Sources/0670-maximum-swap.swift
+++ Sources/0670-maximum-swap.swift
@@ -1,50 +1,36 @@
-import Collections
-
 /// https://leetcode.com/problems/maximum-swap/
 struct MaximumSwap {
   func maximumSwap(_ num: Int) -> Int {
-    var digits: Deque<Int> = []
-    var counts = [Int](repeating: 0, count: 10)
     var remaining = num
+    var digits: [Int] = []
     while remaining > 0 {
-      let digit = remaining%10
-      digits.prepend(digit)
-      counts[digit] += 1
+      digits.append(remaining%10)
       remaining /= 10
     }
 
-    var mismatch: (Int, Int)?
-    var result = [Int](repeating: 0, count: digits.count)
-    var i = 0
-    loop: for (number, count) in counts.enumerated().reversed() {
-      for _ in 0 ..< count {
-        defer { i += 1 }
-        result[i] = number
-        if digits[i] != result[i] {
-          mismatch = (digits[i], result[i])
-          break loop
+    digits.reverse()
+
+    var lastIndex: [Int: Int] = [:]
+    for (index, digit) in digits.enumerated() {
+      lastIndex[digit] = index
+    }
+
+    for (index, digit) in digits.enumerated() {
+      for otherDigit in stride(from: 9, through: digit+1, by: -1) {
+        if let otherIndex = lastIndex[otherDigit], otherIndex > index {
+          digits.swapAt(index, otherIndex)
+
+          var result = 0
+          for digit in digits {
+            result *= 10
+            result += digit
+          }
+
+          return result
         }
       }
     }
 
-    if let (old, new) = mismatch {
-      var j = digits.count-1
-      while j >= i {
-        defer { j -= 1 }
-        if mismatch != nil, digits[j] == new {
-          result[j] = old
-          mismatch = nil
-        } else {
-          result[j] = digits[j]
-        }
-      }
-    }
-
-    var number = 0
-    for digit in result {
-      number = number*10+digit
-    }
-
-    return number
+    return num
   }
 }
