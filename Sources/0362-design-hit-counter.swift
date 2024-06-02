2dfdc4cc1cd9e03131a8ae9b3e68c387262c2acb
Matteo Crespi <crespi.matteo@icloud.com>
2024-06-02 21:08:20 +0200

    362. Design Hit Counter

--- Sources/0362-design-hit-counter.swift
+++ Sources/0362-design-hit-counter.swift
@@ -2,18 +2,25 @@ import Collections
 
 /// https://leetcode.com/problems/design-hit-counter/
 class HitCounter {
-  private var queue: Deque<Int> = []
+  private var count = 0
+  private var queue: Deque<(timestamp: Int, count: Int)> = []
 
   init() {}
 
   func hit(_ timestamp: Int) {
-    queue.append(timestamp)
+    count += 1
+    if queue.last?.timestamp == timestamp {
+      queue[queue.count-1].count += 1
+    } else {
+      queue.append((timestamp, 1))
+    }
   }
 
   func getHits(_ timestamp: Int) -> Int {
-    while let min = queue.first, timestamp-min >= 300 {
+    while let min = queue.first, timestamp-min.timestamp >= 300 {
+      count -= min.count
       queue.removeFirst()
     }
-    return queue.count
+    return count
   }
 }
