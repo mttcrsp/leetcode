1008ed99a230a80f3574922e13b0535e36cd8e0d
Matteo Crespi <crespi.matteo@icloud.com>
2024-06-02 21:02:07 +0200

    127. Word Ladder

--- Sources/0127-word-ladder.swift
+++ Sources/0127-word-ladder.swift
@@ -3,39 +3,37 @@ import Collections
 /// https://leetcode.com/problems/word-ladder/
 struct WordLadder {
   func ladderLength(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> Int {
-    var patterns: [[Character]: Set<String>] = [:]
-    for word in wordList+[beginWord] {
+    func makePatterns(_ word: String) -> [[Character]] {
+      var patterns: [[Character]] = []
       for i in 0 ..< word.count {
         var pattern = Array(word)
         pattern[i] = "*"
-        patterns[pattern, default: []].insert(word)
+        patterns.append(pattern)
+      }
+      return patterns
+    }
+
+    var graph: [[Character]: Set<String>] = [:]
+    for word in wordList+[beginWord] {
+      for pattern in makePatterns(word) {
+        graph[pattern, default: []].insert(word)
       }
     }
 
-    var graph: [String: Set<String>] = [:]
-    for (_, wordList) in patterns where wordList.count > 1 {
-      for i in wordList.indices {
-        for j in wordList.indices where i != j {
-          let wordI = wordList[i]
-          let wordJ = wordList[j]
-          graph[wordI, default: []].insert(wordJ)
-          graph[wordJ, default: []].insert(wordI)
-        }
-      }
-    }
-
-    var visited: Set<String> = []
+    var visited: Set<String> = [beginWord]
     var queue: Deque<(String, Int)> = [(beginWord, 1)]
     while let (word, length) = queue.popFirst() {
-      guard !visited.contains(word) else { continue }
-      visited.insert(word)
-
       if word == endWord {
         return length
       }
 
-      for neighbour in graph[word, default: []] {
-        queue.append((neighbour, length+1))
+      for pattern in makePatterns(word) {
+        for neighbor in graph[pattern, default: []] {
+          if !visited.contains(neighbor) {
+            visited.insert(neighbor)
+            queue.append((neighbor, length+1))
+          }
+        }
       }
     }
 
