75b679a54f559c599e8d83e5c85f1876e72f24e6
Matteo Crespi <crespi.matteo@icloud.com>
2024-06-20 00:21:19 +0200

    921. Minimum Add to Make Parentheses Valid

--- Sources/0921-minimum-add-to-make-parentheses-valid.swift
+++ Sources/0921-minimum-add-to-make-parentheses-valid.swift
@@ -1,18 +1,17 @@
 /// https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
 struct MinimumAddToMakeParenthesesValid {
   func minAddToMakeValid(_ s: String) -> Int {
-    var unmatchedClose = 0
-    var unmatchedOpen = 0
+    var balance = 0, result = 0
     for character in s {
       if character == "(" {
-        unmatchedOpen += 1
-      } else if unmatchedOpen > 0 {
-        unmatchedOpen -= 1
+        balance += 1
+      } else if balance > 0 {
+        balance -= 1
       } else {
-        unmatchedClose += 1
+        result += 1
       }
     }
 
-    return unmatchedClose+unmatchedOpen
+    return result+balance
   }
 }
