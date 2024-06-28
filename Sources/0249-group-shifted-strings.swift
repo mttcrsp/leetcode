8ef0485a895f42c0b37879614bf436cab4bc699d
Matteo Crespi <crespi.matteo@icloud.com>
2024-06-28 19:08:12 +0200

    249. Group Shifted Strings

--- Sources/0249-group-shifted-strings.swift
+++ Sources/0249-group-shifted-strings.swift
@@ -1,24 +1,27 @@
 /// https://leetcode.com/problems/group-shifted-strings/
 class GroupShiftedStrings {
   func groupStrings(_ strings: [String]) -> [[String]] {
+    let a: Character = "a"
+
+    func distances(_ string: String) -> [Int] {
+      var distances: [Int] = []
+      for index in string.indices.dropFirst() {
+        let lhsChar = string[string.index(before: index)]
+        let rhsChar = string[index]
+        let lhsValue = Int(lhsChar.asciiValue!-a.asciiValue!)
+        let rhsValue = Int(rhsChar.asciiValue!-a.asciiValue!)
+        distances.append((26+(rhsValue-lhsValue))%26)
+      }
+
+      return distances
+    }
+
     var groups: [[Int]: [String]] = [:]
     for string in strings {
-      groups[normalize(string), default: []].append(string)
+      let distances = distances(string)
+      groups[distances, default: []].append(string)
     }
 
     return Array(groups.values)
-  }
-
-  func normalize(_ string: String) -> [Int] {
-    let characters = Array(string)
-    var result = [0]
-    for i in characters.indices.dropFirst() {
-      let prevCharacter = characters[i-1]
-      let currCharacter = characters[i]
-      let distance = Int(currCharacter.asciiValue!)-Int(prevCharacter.asciiValue!)
-      result.append((distance+26)%26)
-    }
-
-    return result
   }
 }
