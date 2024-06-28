e2b3564454f524b0ea1bf73f95280cfe1983d2d1
Matteo Crespi <crespi.matteo@icloud.com>
2024-06-28 19:50:54 +0200

    65. Valid Number

--- Sources/0065-valid-number.swift
+++ Sources/0065-valid-number.swift
@@ -1,69 +1,78 @@
 /// https://leetcode.com/problems/valid-number/
 struct ValidNumber {
   func isNumber(_ s: String) -> Bool {
-    var index = s.startIndex
-    if let nextIndex = parseDecimal(s, index) {
+    func scanDigits(_ startIndex: String.Index) -> String.Index? {
+      var index = startIndex, isValid = false
+      while index < s.endIndex, s[index].isNumber {
+        index = s.index(after: index); isValid = true
+      }
+
+      return isValid ? index : nil
+    }
+
+    func scanInteger(_ startIndex: String.Index) -> String.Index? {
+      var index = startIndex
+      if index < s.endIndex, s[index] == "+" || s[index] == "-" {
+        index = s.index(after: index)
+      }
+
+      if let nextIndex = scanDigits(index) {
+        return nextIndex
+      } else {
+        return nil
+      }
+    }
+
+    func scanExponent(_ startIndex: String.Index) -> String.Index? {
+      var index = startIndex
+
+      guard index < s.endIndex, s[index] == "e" || s[index] == "E" else {
+        return nil
+      }
+      index = s.index(after: index)
+
+      if let nextIndex = scanInteger(index) {
+        return nextIndex
+      } else {
+        return nil
+      }
+    }
+
+    func scanDecimal(_ startIndex: String.Index) -> String.Index? {
+      var index = startIndex, isValid = false
+      if index < s.endIndex, s[index] == "+" || s[index] == "-" {
+        index = s.index(after: index)
+      }
+
+      if let nextIndex = scanDigits(index) {
+        index = nextIndex; isValid = true
+      }
+
+      guard index < s.endIndex, s[index] == "." else {
+        return nil
+      }
+      index = s.index(after: index)
+
+      if let nextIndex = scanDigits(index) {
+        index = nextIndex; isValid = true
+      }
+
+      return isValid ? index : nil
+    }
+
+    var index = s.startIndex, isValid = false
+    if let nextIndex = scanDecimal(index) {
       index = nextIndex
-    } else if let nextIndex = parseInteger(s, index) {
+    } else if let nextIndex = scanInteger(index) {
       index = nextIndex
     } else {
       return false
     }
 
-    guard index != s.endIndex else {
-      return true
-    }
-
-    guard s[index] == "e" || s[index] == "E" else {
-      return false
-    }
-
-    index = s.index(after: index)
-    return parseInteger(s, index) == s.endIndex
-  }
-
-  func parseDigits(_ s: String, _ startIndex: String.Index) -> String.Index? {
-    var index = startIndex
-    while index < s.endIndex, s[index].isNumber {
-      index = s.index(after: index)
-    }
-
-    // ensure that you have parsed at least a digit
-    return index == startIndex ? nil : index
-  }
-
-  func parseDecimal(_ s: String, _ startIndex: String.Index) -> String.Index? {
-    var index = startIndex
-    if index < s.endIndex, s[index] == "+" || s[index] == "-" {
-      index = s.index(after: index)
-    }
-
-    if index < s.endIndex, s[index] == "." {
-      index = s.index(after: index)
-      return parseDigits(s, index)
-    }
-
-    if let nextIndex = parseDigits(s, index) {
+    if let nextIndex = scanExponent(index) {
       index = nextIndex
-      if index < s.endIndex, s[index] == "." {
-        index = s.index(after: index)
-        if let nextIndex = parseDigits(s, index) {
-          return nextIndex
-        } else {
-          return index
-        }
-      }
     }
 
-    return nil
-  }
-
-  func parseInteger(_ s: String, _ startIndex: String.Index) -> String.Index? {
-    var index = startIndex
-    if index < s.endIndex, s[index] == "+" || s[index] == "-" {
-      index = s.index(after: index)
-    }
-
-    return parseDigits(s, index)
+    return index == s.endIndex
   }
 }
