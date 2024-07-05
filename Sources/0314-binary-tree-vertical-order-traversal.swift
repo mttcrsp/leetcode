5971fb9fc5ef5913fa89e85dbe33545d5d8e4a61
Matteo Crespi <crespi.matteo@icloud.com>
2024-07-05 16:08:22 +0200

    314. Binary Tree Vertical Order Traversal

--- Sources/0314-binary-tree-vertical-order-traversal.swift
+++ Sources/0314-binary-tree-vertical-order-traversal.swift
@@ -5,25 +5,25 @@ class BinaryTreeVerticalOrderTraversal {
   func verticalOrder(_ root: TreeNode?) -> [[Int]] {
     guard let root else { return [] }
 
-    var minOffset = Int.max
-    var maxOffset = Int.min
-    var queue: Deque<(TreeNode, Int)> = [(root, 0)]
-    var values: [Int: [Int]] = [:]
-    while let (node, offset) = queue.popFirst() {
-      values[offset, default: []].append(node.val)
-      minOffset = min(minOffset, offset)
-      maxOffset = max(maxOffset, offset)
+    var minColumn = 0
+    var maxColumn = 0
+    var columnToValues: [Int: [Int]] = [:]
+    var queue: Deque<(TreeNode, column: Int)> = [(root, 0)]
+    while let (node, column) = queue.popFirst() {
+      minColumn = min(minColumn, column)
+      maxColumn = max(maxColumn, column)
+      columnToValues[column, default: []].append(node.val)
       if let child = node.left {
-        queue.append((child, offset-1))
+        queue.append((child, column-1))
       }
       if let child = node.right {
-        queue.append((child, offset+1))
+        queue.append((child, column+1))
       }
     }
 
     var result: [[Int]] = []
-    for offset in minOffset ... maxOffset {
-      result.append(values[offset]!)
+    for column in minColumn ... maxColumn {
+      result.append(columnToValues[column]!)
     }
 
     return result
