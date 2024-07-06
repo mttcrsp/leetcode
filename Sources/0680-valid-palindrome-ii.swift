bab5aa573f9d62b95dd490faca794ff3dc3ded8a
Matteo Crespi <crespi.matteo@icloud.com>
2024-07-06 12:16:39 +0200

    680. Valid Palindrome II

--- Sources/0680-valid-palindrome-ii.swift
+++ Sources/0680-valid-palindrome-ii.swift
@@ -1,32 +1,31 @@
 /// https://leetcode.com/problems/valid-palindrome-ii/
 struct ValidPalindromeIi {
-  func validPalindrome(_ string: String) -> Bool {
-    var lhs = string.startIndex
-    var rhs = string.index(before: string.endIndex)
-
-    while lhs < rhs, string[lhs] == string[rhs] {
-      lhs = string.index(after: lhs)
-      rhs = string.index(before: rhs)
+  func validPalindrome(_ s: String) -> Bool {
+    var lhs = s.startIndex
+    var rhs = s.index(before: s.endIndex)
+    while lhs < rhs {
+      if s[lhs] == s[rhs] {
+        lhs = s.index(after: lhs)
+        rhs = s.index(before: rhs)
+      } else {
+        return
+          validPalindromeStrict(s[s.index(after: lhs) ... rhs]) ||
+          validPalindromeStrict(s[lhs ... s.index(before: rhs)])
+      }
     }
 
-    if lhs >= rhs {
-      return true
-    }
-
-    let string1 = string[lhs ..< rhs]
-    let string2 = string[string.index(after: lhs) ..< string.index(after: rhs)]
-    return validPalindromeStrict(string1) || validPalindromeStrict(string2)
+    return true
   }
 
-  private func validPalindromeStrict(_ string: Substring) -> Bool {
-    var lhs = string.startIndex
-    var rhs = string.index(before: string.endIndex)
-
-    while lhs < rhs, string[lhs] == string[rhs] {
-      lhs = string.index(after: lhs)
-      rhs = string.index(before: rhs)
+  private func validPalindromeStrict(_ s: Substring) -> Bool {
+    var lhs = s.startIndex
+    var rhs = s.index(before: s.endIndex)
+    while lhs < rhs {
+      guard s[lhs] == s[rhs] else { return false }
+      lhs = s.index(after: lhs)
+      rhs = s.index(before: rhs)
     }
 
-    return lhs >= rhs
+    return true
   }
 }
