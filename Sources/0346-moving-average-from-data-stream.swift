79b4c4829f5e806a3d1d88204fe711d48b288505
Matteo Crespi <crespi.matteo@icloud.com>
2024-07-06 12:49:49 +0200

    346. Moving Average from Data Stream

--- Sources/0346-moving-average-from-data-stream.swift
+++ Sources/0346-moving-average-from-data-stream.swift
@@ -1,23 +1,25 @@
 /// https://leetcode.com/problems/moving-average-from-data-stream/
 class MovingAverage {
-  private var sum = 0
+  private var buffer: [Int] = []
   private var index = 0
-  private var array: [Int?]
+  private var sum = 0
+  private let size: Int
 
   init(_ size: Int) {
-    array = [Int?](repeating: nil, count: size)
+    self.size = size
   }
 
-  func next(_ newValue: Int) -> Double {
-    if let oldValue = array[index] {
-      sum -= oldValue
+  func next(_ val: Int) -> Double {
+    if buffer.count < size {
+      buffer.append(val)
+      sum += val
+    } else {
+      sum -= buffer[index]
+      buffer[index] = val
+      sum += buffer[index]
+      index = (index+1)%size
     }
-    sum += newValue
 
-    array[index] = newValue
-    index = (index+1)%array.count
-
-    let count = array[array.count-1] == nil ? index : array.count
-    return Double(sum)/Double(count)
+    return Double(sum)/Double(buffer.count)
   }
 }
