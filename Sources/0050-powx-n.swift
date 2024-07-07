027ee7d21c82b495d64718faedfe4b8ce6006f74
Matteo Crespi <crespi.matteo@icloud.com>
2024-07-07 15:41:33 +0200

    50. Pow(x, n)

--- Sources/0050-powx-n.swift
+++ Sources/0050-powx-n.swift
@@ -1,19 +1,22 @@
 /// https://leetcode.com/problems/powx-n/
 struct PowxN {
   func myPow(_ x: Double, _ n: Int) -> Double {
+    guard n != 0 else { return 1 }
     guard x != 1 else { return 1 }
 
-    var x = n < 0 ? 1/x : x
+    let base = n > 0 ? x : 1/x
+    var power = Double(base)
+    var result: Double = 1
     var n = abs(n)
-    var pow = 1.0
-    while n != 0 {
+    while n > 0 {
       if n & 1 == 1 {
-        pow *= x
+        result *= power
       }
-      x *= x
-      n >>= 1
+
+      power *= power
+      n = n >> 1
     }
 
-    return pow
+    return result
   }
 }
