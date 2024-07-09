a2981561bc58b921c41398673f10e2acc23a0ba7
Matteo Crespi <crespi.matteo@icloud.com>
2024-07-09 16:09:50 +0200

    26. Remove Duplicates from Sorted Array

--- Sources/0026-remove-duplicates-from-sorted-array.swift
+++ Sources/0026-remove-duplicates-from-sorted-array.swift
@@ -1,9 +1,13 @@
 /// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 struct RemoveDuplicatesFromSortedArray {
   func removeDuplicates(_ nums: inout [Int]) -> Int {
-    for i in nums.indices.reversed().dropFirst() where nums[i] == nums[i+1] {
-      nums.remove(at: i)
+    var index = 1
+    for i in nums.indices.dropFirst() {
+      if nums[i] != nums[i-1] {
+        nums[index] = nums[i]
+        index += 1
+      }
     }
-    return nums.count
+    return index
   }
 }
--- Tests/0026-remove-duplicates-from-sorted-array-tests.swift
+++ Tests/0026-remove-duplicates-from-sorted-array-tests.swift
@@ -5,22 +5,13 @@ import XCTest
 final class RemoveDuplicatesFromSortedArrayTests: XCTestCase {
   func testRemoveDuplicates1() {
     var input = [1, 1, 2]
-    let output = 2
-    XCTAssertEqual(RemoveDuplicatesFromSortedArray().removeDuplicates(&input), output)
-    XCTAssertEqual(input, [1, 2])
+    let output = RemoveDuplicatesFromSortedArray().removeDuplicates(&input)
+    XCTAssertEqual(Array(input.prefix(output)), [1, 2])
   }
 
   func testRemoveDuplicates2() {
-    var input = [1, 1, 2, 2, 3, 3]
-    let output = 3
-    XCTAssertEqual(RemoveDuplicatesFromSortedArray().removeDuplicates(&input), output)
-    XCTAssertEqual(input, [1, 2, 3])
-  }
-
-  func testRemoveDuplicates3() {
-    var input = [1, 2, 3]
-    let output = 3
-    XCTAssertEqual(RemoveDuplicatesFromSortedArray().removeDuplicates(&input), output)
-    XCTAssertEqual(input, [1, 2, 3])
+    var input = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    let output = RemoveDuplicatesFromSortedArray().removeDuplicates(&input)
+    XCTAssertEqual(Array(input.prefix(output)), [0, 1, 2, 3, 4])
   }
 }
