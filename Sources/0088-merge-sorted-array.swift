83dd56b3e2c7997c3b1fc9ab876f1b0f834cfc03
Matteo Crespi <crespi.matteo@icloud.com>
2024-07-09 13:13:04 +0200

    88. Merge Sorted Array

--- Sources/0088-merge-sorted-array.swift
+++ Sources/0088-merge-sorted-array.swift
@@ -1,15 +1,22 @@
 /// https://leetcode.com/problems/merge-sorted-array/
 struct MergeSortedArray {
   func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-    var i = m-1
-    var j = n-1
-    var k: Int { i+j+1 }
-    while j >= 0 {
-      if i >= 0, nums1[i] > nums2[j] {
-        nums1[k] = nums1[i]; i -= 1
+    var index1 = m-1
+    var index2 = n-1
+    var insertionIndex: Int { index1+index2+1 }
+    while index1 >= 0, index2 >= 0 {
+      if nums1[index1] > nums2[index2] {
+        nums1[insertionIndex] = nums1[index1]
+        index1 -= 1
       } else {
-        nums1[k] = nums2[j]; j -= 1
+        nums1[insertionIndex] = nums2[index2]
+        index2 -= 1
       }
+    }
+
+    while index2 >= 0 {
+      nums1[insertionIndex] = nums2[index2]
+      index2 -= 1
     }
   }
 }
