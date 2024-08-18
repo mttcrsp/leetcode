4bb138e02ef961f214ae3c3074850fce3d4c65f4
Matteo Crespi <crespi.matteo@icloud.com>
2024-08-18 13:16:19 +0200

    76. Minimum Window Substring

--- Sources/0076-minimum-window-substring.swift
+++ Sources/0076-minimum-window-substring.swift
@@ -1,37 +1,34 @@
 /// https://leetcode.com/problems/minimum-window-substring/
 struct MinimumWindowSubstring {
   func minWindow(_ s: String, _ t: String) -> String {
+    let s = Array(s)
+
+    var substring: ArraySlice<Character>?
+    var matches = 0
+    var countsS: [Character: Int] = [:]
     var countsT: [Character: Int] = [:]
     for character in t {
       countsT[character, default: 0] += 1
     }
 
-    let s = Array(s)
-    var result: ArraySlice<Character>?
-    var matches = 0
-    var countsS: [Character: Int] = [:]
     var lhs = 0
     for rhs in s.indices {
       countsS[s[rhs], default: 0] += 1
-      if let countT = countsT[s[rhs]], countT == countsS[s[rhs], default: 0] {
-        matches += 1
-      }
+      matches += countsS[s[rhs]] == countsT[s[rhs]] ? 1 : 0
 
       while matches == countsT.count {
-        if result == nil || result!.count > rhs-lhs+1 {
-          result = s[lhs ... rhs]
+        if substring == nil || substring!.count > rhs-lhs+1 {
+          substring = s[lhs ... rhs]
         }
 
+        matches -= countsS[s[lhs]] == countsT[s[lhs]] ? 1 : 0
         countsS[s[lhs], default: 0] -= 1
-        if let countT = countsT[s[lhs]], countT > countsS[s[lhs], default: 0] {
-          matches -= 1
-        }
-        lhs += 1
+        lhs = s.index(after: lhs)
       }
     }
 
-    if let result {
-      return String(result)
+    if let substring {
+      return String(substring)
     } else {
       return ""
     }
