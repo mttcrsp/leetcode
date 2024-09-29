ef65ae11d889de7155b84438993ab72b0a4e0ea2
Matteo Crespi <crespi.matteo@icloud.com>
2024-09-29 19:48:47 +0200

    528. Random Pick with Weight

--- Sources/0528-random-pick-with-weight.swift
+++ Sources/0528-random-pick-with-weight.swift
@@ -1,24 +1,22 @@
 /// https://leetcode.com/problems/random-pick-with-weight/
 struct RandomPickWithWeight {
-  let cumulativeSum: [Int]
+  var runningSums: [Int] = []
 
   init(_ w: [Int]) {
-    var sum = 0
-    var cumulativeSum = w
-    for (i, weight) in w.enumerated() {
-      sum += weight
-      cumulativeSum[i] = sum
+    var runningSum = 0
+    for weight in w {
+      runningSum += weight
+      runningSums.append(runningSum)
     }
-    self.cumulativeSum = cumulativeSum
   }
 
   func pickIndex() -> Int {
-    let target = (1 ... cumulativeSum.last!).randomElement()!
+    let target = (1 ... runningSums.last!).randomElement()!
     var lhs = 0
-    var rhs = cumulativeSum.count-1
+    var rhs = runningSums.count-1
     while lhs < rhs {
       let mid = lhs+(rhs-lhs)/2
-      if cumulativeSum[mid] < target {
+      if runningSums[mid] < target {
         lhs = mid+1
       } else {
         rhs = mid
