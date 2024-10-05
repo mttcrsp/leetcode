ba2178bf5e6241eb19d1c8cbda98679770a53b4d
Matteo Crespi <crespi.matteo@icloud.com>
2024-10-05 19:40:26 +0200

    328. Odd Even Linked List

--- Sources/0328-odd-even-linked-list.swift
+++ Sources/0328-odd-even-linked-list.swift
@@ -1,26 +1,17 @@
 /// https://leetcode.com/problems/odd-even-linked-list/
 struct OddEvenLinkedList {
   func oddEvenList(_ head: ListNode?) -> ListNode? {
-    var evenTail = head
-    var oddTail = evenTail?.next
-    let oddHead = oddTail
-
-    var node = oddTail?.next
-    var isEven = true
-    while let current = node {
-      defer { isEven.toggle() }
-      node = node?.next
-      if isEven {
-        evenTail?.next = current
-        evenTail = current
-      } else {
-        oddTail?.next = current
-        oddTail = current
-      }
+    var odd = head
+    var even = head?.next
+    let evenHead = head?.next
+    while even != nil, even?.next != nil {
+      odd?.next = odd?.next?.next
+      odd = odd?.next
+      even?.next = even?.next?.next
+      even = even?.next
     }
+    odd?.next = evenHead
 
-    evenTail?.next = oddHead
-    oddTail?.next = nil
     return head
   }
 }
