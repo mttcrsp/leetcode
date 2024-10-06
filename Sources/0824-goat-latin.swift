1ec739ffc73ceac92df229d9068e48a9bd57c9f4
Matteo Crespi <crespi.matteo@icloud.com>
2024-10-06 14:10:11 +0200

    824. Goat Latin

--- Sources/0824-goat-latin.swift
+++ Sources/0824-goat-latin.swift
@@ -1,22 +1,22 @@
 /// https://leetcode.com/problems/goat-latin/
 struct GoatLatin {
-  func toGoatLatin(_ string: String) -> String {
-    var words = string.components(separatedBy: " ")
+  func toGoatLatin(_ sentence: String) -> String {
+    let vowels: Set<Character> = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
-    for index in words.indices {
-      guard let initial = words[index].first else { continue }
+    var goatLatinWords: [String] = []
+    for (index, var word) in sentence.components(separatedBy: " ").enumerated() {
+      guard let initial = word.first else { continue }
 
-      let vowels: Set<Character> = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-      if vowels.contains(initial) {
-        words[index] += "ma"
-      } else {
-        words[index].removeFirst()
-        words[index] += "\(initial)ma"
+      if !vowels.contains(initial) {
+        word.removeFirst()
+        word.append(initial)
       }
 
-      words[index] += String(repeating: "a", count: index+1)
+      word.append("ma")
+      word.append(String(repeating: "a", count: index+1))
+      goatLatinWords.append(word)
     }
 
-    return words.joined(separator: " ")
+    return goatLatinWords.joined(separator: " ")
   }
 }
