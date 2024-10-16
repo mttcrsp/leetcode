9c552e3bf8bc48b4c9483abfc93791ad3a354ea4
Matteo Crespi <crespi.matteo@icloud.com>
2024-10-17 01:44:38 +0200

    1768. Merge Strings Alternately

--- Sources/1768-merge-strings-alternately.swift
+++ Sources/1768-merge-strings-alternately.swift
@@ -1,25 +1,19 @@
 /// https://leetcode.com/problems/merge-strings-alternately/
 struct MergeStringsAlternately {
   func mergeAlternately(_ word1: String, _ word2: String) -> String {
-    var i = word1.startIndex
-    var j = word2.startIndex
     var result = ""
-    while i < word1.endIndex, j < word2.endIndex {
-      if i > j {
-        result.append(word2[j])
-        j = word2.index(after: j)
-      } else {
-        result.append(word1[i])
-        i = word1.index(after: i)
+    var index1 = word1.startIndex
+    var index2 = word2.startIndex
+    while index1 < word1.endIndex || index2 < word2.endIndex {
+      if index1 < word1.endIndex {
+        result.append(word1[index1])
+        index1 = word1.index(after: index1)
+      }
+      if index2 < word2.endIndex {
+        result.append(word2[index2])
+        index2 = word2.index(after: index2)
       }
     }
-
-    if i < word1.endIndex {
-      result.append(contentsOf: word1[i...])
-    } else if j < word2.endIndex {
-      result.append(contentsOf: word2[j...])
-    }
-
     return result
   }
 }
