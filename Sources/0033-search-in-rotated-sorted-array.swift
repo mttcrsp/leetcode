d6745aa388dd6467874cca389290140f35dcffd9
Matteo Crespi <crespi.matteo@icloud.com>
2024-10-17 01:19:42 +0200

    33. Search in Rotated Sorted Array

--- Sources/0033-search-in-rotated-sorted-array.swift
+++ Sources/0033-search-in-rotated-sorted-array.swift
@@ -1,35 +1,45 @@
 /// https://leetcode.com/problems/search-in-rotated-sorted-array/
 struct SearchInRotatedSortedArray {
   func search(_ nums: [Int], _ target: Int) -> Int {
-    var lhs = 0
-    var rhs = nums.count-1
-    while lhs < rhs-1 {
-      let mid = (lhs+rhs+1)/2
-      if nums[mid] == target {
-        return mid
-      }
-
-      if nums[lhs] < nums[mid] {
-        if nums[lhs] ... nums[mid] ~= target {
-          rhs = mid
-        } else {
-          lhs = mid+1
-        }
-      } else {
-        if nums[mid] ... nums[rhs] ~= target {
+    func binarySearchPivot() -> Int? {
+      var lhs = 0
+      var rhs = nums.count-1
+      if nums[lhs] < nums[rhs] { return nil }
+      while lhs < rhs {
+        let mid = (lhs+rhs+1)/2
+        if nums[mid] > nums[0] {
           lhs = mid
         } else {
           rhs = mid-1
         }
       }
+      return lhs
     }
 
-    if nums[lhs] == target {
-      return lhs
-    } else if nums[rhs] == target {
-      return rhs
+    func binarySearch(_ lhs: Int, _ rhs: Int) -> Int? {
+      var lhs = lhs
+      var rhs = rhs
+      while lhs <= rhs {
+        let mid = (lhs+rhs)/2
+        if nums[mid] < target {
+          lhs = mid+1
+        } else if nums[mid] > target {
+          rhs = mid-1
+        } else {
+          return mid
+        }
+      }
+      return nil
+    }
+
+    guard let pivot = binarySearchPivot() else {
+      return binarySearch(0, nums.count-1) ?? -1
+    }
+
+    if nums[0] ... nums[pivot] ~= target {
+      return binarySearch(0, pivot) ?? -1
     } else {
-      return -1
+      return binarySearch(pivot+1, nums.count-1) ?? -1
     }
   }
 }
