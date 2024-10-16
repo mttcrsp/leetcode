4262ec109635c05809b898aa61d38167bf00d569
Matteo Crespi <crespi.matteo@icloud.com>
2024-10-17 01:33:59 +0200

    78. Subsets

--- Sources/0078-subsets.swift
+++ Sources/0078-subsets.swift
@@ -1,19 +1,20 @@
 /// https://leetcode.com/problems/subsets/
 struct Subsets {
   func subsets(_ nums: [Int]) -> [[Int]] {
-    var result: [[Int]] = [[]]
-
-    func visit(_ offset: Int = 0, subset: [Int] = []) {
-      if offset < nums.count {
-        let extendedSubset = subset+[nums[offset]]
-        result.append(extendedSubset)
-        visit(offset+1, subset: extendedSubset)
-        visit(offset+1, subset: subset)
+    var result: [[Int]] = []
+    var subset: [Int] = []
+    func backtrack(_ index: Int = 0) {
+      if index < nums.count {
+        subset.append(nums[index])
+        backtrack(index+1)
+        subset.removeLast()
+        backtrack(index+1)
+      } else {
+        result.append(subset)
       }
     }
 
-    visit()
-
+    backtrack()
     return result
   }
 }
