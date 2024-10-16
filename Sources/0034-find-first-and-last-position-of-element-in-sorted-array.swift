669f872b969905f5de8284f6a7f28de3b1821f18
Matteo Crespi <crespi.matteo@icloud.com>
2024-10-17 00:15:14 +0200

    34. Find First and Last Position of Element in Sorted Array

--- Sources/0034-find-first-and-last-position-of-element-in-sorted-array.swift
+++ Sources/0034-find-first-and-last-position-of-element-in-sorted-array.swift
@@ -1,35 +1,38 @@
 /// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 struct FindFirstAndLastPositionOfElementInSortedArray {
   func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
-    var result: [Int] = []
+    var result = [-1, -1]
+    guard !nums.isEmpty else { return result }
+
     var lhs = 0
     var rhs = nums.count-1
-    while lhs <= rhs {
+    while lhs < rhs {
       let mid = (lhs+rhs)/2
       if nums[mid] < target {
         lhs = mid+1
-      } else {
+      } else if nums[mid] > target {
         rhs = mid-1
+      } else {
+        rhs = mid
       }
     }
 
-    guard nums.indices ~= lhs, nums[lhs] == target else { return [-1, -1] }
-    result.append(lhs)
+    guard nums[lhs] == target else { return result }
+    result[0] = lhs
 
-    lhs = 0
     rhs = nums.count-1
-    while lhs <= rhs {
-      let mid = (lhs+rhs)/2
-      if nums[mid] > target {
+    while lhs < rhs {
+      let mid = (lhs+rhs+1)/2
+      if nums[mid] < target {
+        lhs = mid+1
+      } else if nums[mid] > target {
         rhs = mid-1
       } else {
-        lhs = mid+1
+        lhs = mid
       }
     }
 
-    guard nums.indices ~= rhs, nums[rhs] == target else { return [-1, -1] }
-    result.append(rhs)
-
+    result[1] = lhs
     return result
   }
 }
