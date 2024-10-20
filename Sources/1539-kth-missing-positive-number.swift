338dc7f0001f4ee5e7ade91cf74b219b31f71199
Matteo Crespi <crespi.matteo@icloud.com>
2024-10-20 13:45:56 +0200

    1539. Kth Missing Positive Number

--- Sources/1539-kth-missing-positive-number.swift
+++ Sources/1539-kth-missing-positive-number.swift
@@ -1,17 +1,28 @@
 /// https://leetcode.com/problems/kth-missing-positive-number/
 struct KthMissingPositiveNumber {
   func findKthPositive(_ arr: [Int], _ k: Int) -> Int {
+    func missingPositives(at index: Int) -> Int {
+      arr[index]-index-1
+    }
+
     var lhs = 0
     var rhs = arr.count-1
-    while lhs <= rhs {
+    while lhs < rhs {
       let mid = (lhs+rhs)/2
-      if arr[mid]-mid-1 < k {
-        lhs = mid+1
+      if missingPositives(at: mid) >= k {
+        rhs = mid
       } else {
-        rhs = mid-1
+        lhs = mid+1
       }
     }
 
-    return lhs+k
+    let missing = missingPositives(at: lhs)
+    if missing > k {
+      return arr[lhs]-(missing-k)-1
+    } else if missing == k {
+      return arr[lhs]-1
+    } else {
+      return arr[lhs]+(k-missing)
+    }
   }
 }
