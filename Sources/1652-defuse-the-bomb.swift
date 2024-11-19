95d3d5dd8c42c2e09b719f3e6bd19ee0e1a86af8
Matteo Crespi <crespi.matteo@icloud.com>
2024-11-19 01:28:18 +0100

    1652. Defuse the Bomb

--- Sources/1652-defuse-the-bomb.swift
+++ Sources/1652-defuse-the-bomb.swift
@@ -1,17 +1,22 @@
 /// https://leetcode.com/problems/defuse-the-bomb/
 struct DefuseTheBomb {
   func decrypt(_ code: [Int], _ k: Int) -> [Int] {
+    var result = [Int](repeating: 0, count: code.count)
+    guard k != 0 else { return result }
+
+    var lhs = k > 0 ? 1 : code.count-abs(k)
+    var rhs = k > 0 ? k : code.count-1
     var sum = 0
-    for value in code {
-      sum += value
+    for index in lhs ... rhs {
+      sum += code[index]
     }
-    
-    var result = [Int](repeating: sum, count: code.count)
-    for offset in 0 ..< code.count-abs(k) {
-      for index in result.indices {
-        let targetIndex = k > 0 ? index-offset : index+offset
-        result[index] -= code[(targetIndex+code.count)%code.count]
-      }
+
+    for index in 0 ..< code.count {
+      result[index] = sum
+      sum -= code[lhs]
+      lhs = (lhs+1)%code.count
+      rhs = (rhs+1)%code.count
+      sum += code[rhs]
     }
 
     return result
